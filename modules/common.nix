9fb9c96a226d1355edb46cbaf1e7ac5b309bef36
Aljam <aljam@live.ca>
2026-07-28 13:39:06 -0400

    Update SSH authorized keys for user aljam

--- modules/common.nix
+++ modules/common.nix
@@ -53,7 +53,7 @@
   users.users.aljam = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI... aljam@navi" # Put your actual public key here!
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzrMiro1XD8krk5Kb4EWQ+rGjmgKXha/OuOmUZcopRL navi-desktop"
     ];
   };
 
