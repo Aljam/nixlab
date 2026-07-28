bba5be8f26e60d96868a4c7c023eceb878fa8a58
Aljam <aljam@live.ca>
2026-07-28 09:14:34 -0400

    Add OBS Studio configuration to home.nix
    Added OBS Studio configuration with plugins.

--- users/aljam/home.nix
+++ users/aljam/home.nix
@@ -80,5 +80,20 @@
     };
   };
 
+  # OBS Studio Configuration
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+      input-overlay
+      obs-command-source
+      obs-retro-effects
+    ];
+  };
+
   programs.home-manager.enable = true;
 }
