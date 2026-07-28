5790dff452819c593ff96f6e393f270ceb231ed6
Aljam <aljam@live.ca>
2026-07-28 09:19:37 -0400

    Add desktop programs and gaming configurations

---
+++ modules/desktop.nix
@@ -0,0 +1,15 @@
+  ### Desktop Programs & Gaming
+  programs = {
+    kdeconnect.enable = true;
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+      protontricks.enable = true;
+    };
+  };
