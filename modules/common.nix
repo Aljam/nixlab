77cf3844a346054da1972c54a45dcf7973e591f6
Aljam <aljam@live.ca>
2026-07-28 09:21:37 -0400

    Add desktop programs and gaming configurations
    Enable desktop programs and gaming features including KDE Connect and Steam with additional configurations.

--- modules/common.nix
+++ modules/common.nix
@@ -27,4 +27,21 @@
     enable = true;
     powerOnBoot = true;
   };
+
+  ### Desktop Programs & Gaming
+  programs = {
+    kdeconnect.enable = true;
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
+      protontricks.enable = true;
+    };
+  };
+
 }
