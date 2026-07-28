856fd143200a00ceb31e8ffbf6ccad68875852c4
Aljam <aljam@live.ca>
2026-07-28 10:01:02 -0400

    Add Nix settings for experimental features

--- flake.nix
+++ flake.nix
@@ -70,6 +70,12 @@
       };
     };
 
+    nix.settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      substituters = [ "https://cache.nixos-cuda.org" ];
+      trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+    };
+
     homeConfigurations.aljam = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system};
       extraSpecialArgs = { inherit inputs; };
