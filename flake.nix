e45b5193dd1c3f4ceaf37f17851dea237172a514
Aljam <aljam@live.ca>
2026-07-28 14:16:57 -0400

    Refactor home-manager input in flake.nix
    Removed redundant home-manager input declaration.

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       disko.url = "github:nix-community/disko";
       disko.inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
+      home-manager = {
+        url = "github:nix-community/home-manager/release-25.11";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
