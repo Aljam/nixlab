a52ad990384b55601c3fed0ed4dc83c25d484d36
Aljam <aljam@live.ca>
2026-07-28 07:40:31 -0400

    Refactor common.nix for system configuration
    Updated Nix configuration to include timezone, locale, and enable Flakes.

--- modules/common.nix
+++ modules/common.nix
@@ -1,6 +1,20 @@
-# Inside modules/common.nix
-environment.systemPackages = with pkgs; [
-  kitty.terminfo
-  htop
-  git
-];
+{ config, pkgs, ... }:
+
+{
+  # Allow unfree packages fleet-wide
+  nixpkgs.config.allowUnfree = true;
+
+  # Set your global timezone and locale
+  time.timeZone = "America/Toronto"; # Adjust to your timezone
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Essential system packages needed on all machines
+  environment.systemPackages = with pkgs; [
+    git
+    htop
+    kitty.terminfo # Fixes SSH terminal issues when connecting from Kitty
+  ];
+
+  # Enable Flakes globally on all systems
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
