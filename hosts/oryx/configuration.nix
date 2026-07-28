3669f336291ea3f7b71be0540c0d0474acb9b755
Aljam <aljam@live.ca>
2026-07-28 09:21:01 -0400

    Remove desktop programs and gaming settings
    Removed desktop programs and gaming configurations.

--- hosts/oryx/configuration.nix
+++ hosts/oryx/configuration.nix
@@ -80,22 +80,6 @@
   };
   powerManagement.cpuFreqGovernor = "performance";
 
-  ### Desktop Programs & Gaming
-  programs = {
-    kdeconnect.enable = true;
-    gamemode.enable = true;
-    steam = {
-      enable = true;
-      remotePlay.openFirewall = true;
-      dedicatedServer.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraCompatPackages = with pkgs; [
-        proton-ge-bin
-      ];
-      protontricks.enable = true;
-    };
-  };
-
   users.users.aljam = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
