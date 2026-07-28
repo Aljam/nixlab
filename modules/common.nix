021c395709fcc91974a38b8a70549e34b0adf18c
Aljam <aljam@live.ca>
2026-07-28 13:17:54 -0400

    Remove Python and Node.js package management
    Removed Python and Node.js packages from common.nix.

--- modules/common.nix
+++ modules/common.nix
@@ -22,17 +22,6 @@
     vim
     cifs-utils
     kitty.terminfo # Fixes SSH terminal issues when connecting from Kitty
-
-    # Python & Pip Management
-    python3
-    python3Packages.pip
-    python3Packages.virtualenv
-    python3Packages.setuptools
-    python3Packages.wheel
-
-    nodejs  
-    nodePackages.npm
-
     lm_sensors
     iotop
     nvd
