eb8e5de40d59a678049753ba1b8c9b73ba8e244f
Aljam <aljam@live.ca>
2026-07-28 10:12:26 -0400

    Remove Python & Pip management packages
    Removed Python and Pip management packages from configuration.

--- users/aljam/home.nix
+++ users/aljam/home.nix
@@ -15,13 +15,6 @@
     jq
     tldr
 
-    # Python & Pip Management
-    python3
-    python3Packages.pip
-    python3Packages.virtualenv
-    python3Packages.setuptools
-    python3Packages.wheel
-
     # GUI Applications & Software
     ungoogled-chromium
     kdePackages.kate
