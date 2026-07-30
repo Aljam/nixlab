d958a5cc6b7f60cd98b63556ee32808f6e91622f
Aljam <aljam@live.ca>
2026-07-30 17:57:46 -0400

    Refactor bootloader settings in configuration.nix
    Reorganize bootloader settings for ultrawide monitor support and maintain existing configurations.

--- hosts/navi/configuration.nix
+++ hosts/navi/configuration.nix
@@ -1,43 +1,30 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-imports = [
-    ./hardware-configuration.nix
-    ../../modules/common.nix
-    ../../modules/desktop.nix    # Gets you KDE, Steam, Audio, etc.
-    ../../modules/aljam.nix # Gets you your identity
-    ../../modules/libvirt.nix    # Gets you VMs!
-    ../../modules/nas-mount.nix  # Gets you your network share!
-  ];
-
-  networking.hostName = "navi";
-
-  ### Bootloader (GRUB)
-  boot.loader = {
-    timeout = 10;
-    systemd-boot.enable = false;
-    efi.canTouchEfiVariables = true;
-    grub = {
-      enable = true;
-      efiSupport = true;
-      efiInstallAsRemovable = false;
-      device = "nodev";
-      gfxmodeEfi = "3440x1440";
-      gfxmodeBios = "3440x1440";
-      gfxpayloadEfi = "keep";
-      configurationLimit = 10;
-      timeoutStyle = "menu";
-    };
-  };
-
-  ### External USB Enclosure Quirks
-  boot.extraModprobeConfig = "options usb-storage use_uas=0";
-  boot.kernelParams = [ 
-    "usb-storage.quirks=152d:0551:u"
-    "usbcore.autosuspend=-1"
-  ];
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  system.stateVersion = "23.11";
+    imports = [
+        ./hardware-configuration.nix
+        ../../modules/common.nix
+        ../../modules/desktop.nix    # Gets you KDE, Steam, Audio, etc.
+        ../../modules/aljam.nix # Gets you your identity
+        ../../modules/libvirt.nix    # Gets you VMs!
+        ../../modules/nas-mount.nix  # Gets you your network share!
+      ];
+    
+    networking.hostName = "navi";
+    
+    # --- Navi-Specific Bootloader Overrides (Ultrawide Monitor) ---
+    boot.loader.grub.gfxmodeEfi = "3440x1440";
+    boot.loader.grub.gfxmodeBios = "3440x1440";
+    boot.loader.grub.gfxpayloadEfi = "keep";
+    
+    ### External USB Enclosure Quirks
+    boot.extraModprobeConfig = "options usb-storage use_uas=0";
+    boot.kernelParams = [ 
+        "usb-storage.quirks=152d:0551:u"
+        "usbcore.autosuspend=-1"
+    ];
+    
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+    
+    system.stateVersion = "23.11";
 }
