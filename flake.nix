c27bf841ccfa197537674f5a3f109d4b004f805f
Aljam <aljam@live.ca>
2026-07-30 19:50:57 -0400

    Add backup file extension to home-manager config

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.users.aljam = import ./users/aljam/home.nix;
         }
       ] ++ extraModules;
