e4213f797e69a8bff0026dff7119fe16b9fba55d
Aljam <aljam@live.ca>
2026-08-01 14:57:58 +0000

    Clean up configuration.nix by removing specific settings
    Removed Navi-specific bootloader settings and USB enclosure quirks.

--- hosts/navi/configuration.nix
+++ hosts/navi/configuration.nix
@@ -6,21 +6,8 @@
     	../../modules/roles/desktop-node.nix
     	../../modules/features/graphics.nix
     	../../modules/features/libvirt.nix
+        ../../modules/hardware/navi-desktop.nix
       ];
     
     networking.hostName = "navi";
-    
-    # --- Navi-Specific Bootloader Overrides (Ultrawide Monitor) ---
-    boot.loader.grub.gfxmodeEfi = "3440x1440";
-    boot.loader.grub.gfxmodeBios = "3440x1440";
-    boot.loader.grub.gfxpayloadEfi = "keep";
-    
-    ### External USB Enclosure Quirks
-    boot.extraModprobeConfig = "options usb-storage use_uas=0";
-    boot.kernelParams = [ 
-        "usb-storage.quirks=152d:0551:u"
-        "usbcore.autosuspend=-1"
-    ];
-    
-    boot.kernelPackages = pkgs.linuxPackages_latest;
 }
