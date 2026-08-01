c2852469af87a38afca7dfb7573b3f750490b84b
Aljam <aljam@live.ca>
2026-08-01 20:12:05 +0000

    Change Grafana HTTP address to allow external access

--- modules/features/observability.nix
+++ modules/features/observability.nix
@@ -25,7 +25,7 @@
     enable = true;
     settings = {
       server = {
-        http_addr = "127.0.0.1";
+        http_addr = "0.0.0.0";
         http_port = 3000;
         domain = "https://grafana.derezzed.info"; 
       };
