b7b3f6d96922139f5a1599fbe64bbd2464ee85d8
Aljam <aljam@live.ca>
2026-08-01 16:36:58 +0000

    Refactor media services configuration in arr-stack

--- modules/features/arr-stack.nix
+++ modules/features/arr-stack.nix
@@ -1,21 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  users.users.autobrr = {
-    isSystemUser = true;
-    group = "media";
-  };
-
-  users.groups.media = {};
-
-  # Define the SOPS secret
-  sops.secrets."autobrr.env" = {
-    sopsFile = ../../secrets/autobrr.enc.env;
-    format = "dotenv";
-    owner = "autobrr";
-    group = "media";
-  };
-
   # Directory structures for media
   systemd.tmpfiles.rules = [
     "d /mnt/media/movies 0770 root media -"
@@ -23,25 +8,74 @@
     "d /mnt/media/downloads 0770 root media -"
   ];
 
-  services.bazarr = { enabled = true; openFirewall = true; port = 6767; };
-  services.sonarr = { enable = true; openFirewall = true; group = "media"; settings.server.bindAddress = "0.0.0.0"; };
-  services.radarr = { enable = true; openFirewall = true; group = "media"; settings.server.bindAddress = "0.0.0.0"; };
-  services.readarr = { enable = true; openFirewall = true; port = 8787; };
-  services.prowlarr = { enable = true; openFirewall = true; settings.server.bindAddress = "0.0.0.0"; };
-  services.lidarr = { enable = true; openFirewall = true; port = 8686; };
-  services.seerr = { enable = true; openFirewall = true; };
+# --- SOPS Secret for Autobrr ---
+  sops.secrets."autobrr.env" = {
+    sopsFile = ../../secrets/autobrr.enc.env;
+    format = "dotenv";
+    owner = "autobrr";
+    group = "media";
+  };
+
+  # --- Core Video & Indexing Stack ---
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+    port = 8989;
+  };
+
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+    port = 7878;
+  };
+
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+    port = 9696;
+  };
+
+  services.jellyseerr = {
+    enable = true;
+    openFirewall = true;
+    port = 5055;
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    port = 8080;
+  };
+
+  # --- Extended Media Library Stack ---
+  services.bazarr = {
+    enable = true;
+    openFirewall = true;
+    port = 6767;
+  };
+
+  services.readarr = {
+    enable = true;
+    openFirewall = true;
+    port = 8787;
+  };
+
+  services.lidarr = {
+    enable = true;
+    openFirewall = true;
+    port = 8686;
+  };
+
+  services.audiobookshelf = {
+    enable = true;
+    openFirewall = true;
+    port = 13378;
+  };
   
   services.autobrr = {
     enable = true;
     secretFile = config.sops.secrets."autobrr.env".path;
     settings = { port = 7474; host = "0.0.0.0"; };
-  };
-
-  # --- Audiobookshelf (Audiobooks & Podcasts) ---
-  services.audiobookshelf = {
-    enable = true;
-    port = 13378;
-    openFirewall = true;
   };
   
   services.recyclarr.enable = true;
