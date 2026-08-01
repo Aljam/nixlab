6c89f4621882ef289b3d4dc2f3bf5ffa88cee748
Aljam <aljam@live.ca>
2026-08-01 12:55:42 +0000

    Add Jellyfin service configuration with hardware support

---
+++ modules/features/jellyfin.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  users.groups.media = {};
+
+  services.jellyfin.enable = true;
+  services.jellyfin.openFirewall = true;
+  services.jellyfin.group = "media";
+
+  # Hardware acceleration passthrough for Jellyfin
+  systemd.services.jellyfin.serviceConfig = {
+    SupplementaryGroups = [ "media" "video" "render" ];
+    DeviceAllow = [
+      "/dev/nvidia0 rwm"
+      "/dev/nvidiactl rwm"
+      "/dev/nvidia-uvm rwm"
+      "/dev/nvidia-uvm-tools rwm"
+      "char-drm rwm"
+    ];
+  };
+}
