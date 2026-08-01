82128cbce3f88549db36ee9a43a8472c80100ba3
Aljam <aljam@live.ca>
2026-08-01 16:41:38 +0000

    Add new media and automation services to configuration

--- modules/roles/media-node.nix
+++ modules/roles/media-node.nix
@@ -18,6 +18,7 @@
         "Media & Requests" = [
           { Jellyfin = { href = "https://jellyfin.derezzed.info"; description = "Media Streaming"; icon = "jellyfin.png"; }; }
           { Seerr = { href = "https://seerr.derezzed.info"; description = "Media Requests"; icon = "seerr.png"; }; }
+          { Audiobookshelf = { href = "https://audiobookshelf.derezzed.info"; description = "Audiobooks & Podcasts"; icon = "audiobookshelf.png"; }; }
         ];
       }
       {
@@ -25,8 +26,11 @@
           { Sonarr = { href = "https://sonarr.derezzed.info"; description = "TV Shows"; icon = "sonarr.png"; }; }
           { Radarr = { href = "https://radarr.derezzed.info"; description = "Movies"; icon = "radarr.png"; }; }
           { Prowlarr = { href = "https://prowlarr.derezzed.info"; description = "Indexers"; icon = "prowlarr.png"; }; }
-          # Using localhost instead of hardcoded IP so this works on any server
+          { Bazarr = { href = "https://bazarr.derezzed.info"; description = "Subtitles Automation"; icon = "bazarr.png"; }; }
+          { Readarr = { href = "https://readarr.derezzed.info"; description = "Books Automation"; icon = "readarr.png"; }; }
+          { Lidarr = { href = "https://lidarr.derezzed.info"; description = "Music Automation"; icon = "lidarr.png"; }; }
           { qBittorrent = { href = "http://127.0.0.1:8080"; description = "Torrents"; icon = "qbittorrent.png"; }; }
+          { Autobrr = { href = "https://autobrr.derezzed.info"; description = "IRC Torrent Filters"; icon = "autobrr.png"; }; }
         ];
       }
       {
