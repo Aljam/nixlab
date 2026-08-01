624b3cd81a062bb9914bce8a0868cb406f075430
Aljam <aljam@live.ca>
2026-08-01 15:23:56 +0000

    Change dataset name from 'tank/media' to 'mnt/media'

--- modules/features/sanoid.nix
+++ modules/features/sanoid.nix
@@ -18,7 +18,7 @@
 
     # Apply templates to your ZFS pools (Adjust pool/dataset names to match yours)
     datasets = {
-      "tank/media" = {
+      "mnt/media" = {
         useTemplate = "storage";
         recursive = true;
       };
