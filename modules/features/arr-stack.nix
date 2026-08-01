6d5b89f0a0953f773520e7f2d2e626dbdf97353b
Aljam <aljam@live.ca>
2026-08-01 16:25:53 +0000

    Add services for Bazarr and Audiobookshelf

--- modules/features/arr-stack.nix
+++ modules/features/arr-stack.nix
@@ -23,15 +23,25 @@
     "d /mnt/media/downloads 0770 root media -"
   ];
 
+  services.bazarr = { enabled = true; openFirewall = true; port = 6767; };
   services.sonarr = { enable = true; openFirewall = true; group = "media"; settings.server.bindAddress = "0.0.0.0"; };
   services.radarr = { enable = true; openFirewall = true; group = "media"; settings.server.bindAddress = "0.0.0.0"; };
+  services.readarr = { enable = true; openFirewall = true; port = 8787; };
   services.prowlarr = { enable = true; openFirewall = true; settings.server.bindAddress = "0.0.0.0"; };
+  services.lidarr = { enable = true; openFirewall = true; port = 8686; };
   services.seerr = { enable = true; openFirewall = true; };
   
   services.autobrr = {
     enable = true;
     secretFile = config.sops.secrets."autobrr.env".path;
     settings = { port = 7474; host = "0.0.0.0"; };
+  };
+
+  # --- Audiobookshelf (Audiobooks & Podcasts) ---
+  services.audiobookshelf = {
+    enable = true;
+    port = 13378;
+    openFirewall = true;
   };
   
   services.recyclarr.enable = true;
