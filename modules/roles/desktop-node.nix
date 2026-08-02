075f4f07124976e8ddb217ea12708bacc33d1e54
Aljam <aljam@live.ca>
2026-08-02 00:09:10 -0400

    Remove gaming architecture settings
    Removed gaming architecture settings from desktop-node configuration.

--- modules/roles/desktop-node.nix
+++ modules/roles/desktop-node.nix
@@ -36,20 +36,6 @@
   # --- Desktop Programs & Device Integration ---
   programs.kdeconnect.enable = true;
 
-  # --- Gaming Architecture ---
-  programs.gamemode.enable = true;
-  
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    protontricks.enable = true;
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
-  };
-
   # Enable Pipewire for modern audio
   security.rtkit.enable = true;
   services.pipewire = {
