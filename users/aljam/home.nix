23ed084676d9d0eba7b3b45fc03dcdb154808f3e
Aljam <aljam@live.ca>
2026-08-02 17:46:05 -0400

    Modify fish greeting with ASCII art
    Updated the fish greeting to include an ASCII art design.

--- users/aljam/home.nix
+++ users/aljam/home.nix
@@ -24,7 +24,25 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      set -g fish_greeting ""
+      set -g fish_greeting "${c1}
+                      .@hpp
+                      "KKP
+            ,      ,+;n@nnw,    ,,
+         ,||ppKN|||p##KKNpp|LKp!L@|IN,
+       ;L##KKPL|$#KKM"````"TE|IKm`"K@LKp
+     ||#KKM` ||#KKP .*||hw,  T|I#N   T||Km
+    ||$#K`   |I#KP | |||$##N  @I#KH   ||$#N
+    '|I8N    |I#Kb Y@@p##KBKP |I#KN  ,@$#KM
+      TPLKm, Y|I8N  "KKKKKK* ||##KL,@$#KKP
+        "Kp@bp$p@LKp,     .||p##KH@p#KK*
+     U@#Kp`*KKK*Tb@@I#m |.{#KKKM*KKKPT@#Kp
+      `*"         `PK#K HI#KM`        `**`
+                   IK#K $K#B
+           ,Kpp    I#BB IhKKp   p#p,
+           "KKKKmp##KKK TKKKKp##KKKM
+             `TKKKKKKP`  `*KKKKKKM`
+
+"
     '';
     shellAliases = {
       ls = "eza --icons";
