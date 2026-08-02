d0badc8bd322b00afb19b47d8b36f7bee4d60cf6
Aljam <aljam@live.ca>
2026-08-02 00:34:11 -0400

    Add 'input' and 'plugdev' to extraGroups

--- users/aljam/nixos.nix
+++ users/aljam/nixos.nix
@@ -8,7 +8,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzrMiro1XD8krk5Kb4EWQ+rGjmgKXha/OuOmUZcopRL navi-desktop"
     ];
-    extraGroups = [ "networkmanager" "wheel" "docker" "video" "render" "media" ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "video" "render" "media" "input" "plugdev" ];
     shell = pkgs.fish; # Or whichever shell you use
   };
 }
