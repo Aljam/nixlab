acdd13d11a58e0e3f868efda9bdf70c46ff961b9
Aljam <aljam@live.ca>
2026-08-02 18:06:49 -0400

    Clean up navi-desktop.nix configuration
    Removed comments related to bootloader overrides and USB enclosure quirks.

--- modules/hardware/navi-desktop.nix
+++ modules/hardware/navi-desktop.nix
@@ -1,17 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-  # --- Navi-Specific Bootloader Overrides (Ultrawide Monitor) ---
   boot.loader.grub.gfxmodeEfi = "3440x1440";
   boot.loader.grub.gfxmodeBios = "3440x1440";
   boot.loader.grub.gfxpayloadEfi = "keep";
 
-  # --- External USB Enclosure Quirks ---
   boot.extraModprobeConfig = "options usb-storage use_uas=0";
   boot.kernelParams = [ 
     "usb-storage.quirks=152d:0551:u"
     "usbcore.autosuspend=-1"
   ];
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
