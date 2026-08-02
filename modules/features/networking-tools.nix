8a3ff38751c6edbc61450878753ac5b742eaf035
Aljam <aljam@live.ca>
2026-08-02 17:59:48 -0400

    Refactor comments and clean up package list

--- modules/features/networking-tools.nix
+++ modules/features/networking-tools.nix
@@ -1,23 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  # This enables the Wireshark GUI and, more importantly, configures the 
-  # setcap wrappers so your user can capture packets without running as root.
+  # Configures setcap wrappers to allow packet capture without root
   programs.wireshark.enable = true;
 
   environment.systemPackages = with pkgs; [
-    # Core Network Analysis
     nmap
     tcpdump
-    
-    # Troubleshooting & Benchmarking
-    mtr        # A much better, real-time traceroute
-    iperf3     # For testing actual LAN speeds between your servers
-    ipcalc     # Handy for calculating subnets
+    mtr
+    iperf3
+    ipcalc
 
-    # Network Visualization & Simulation
     gns3-gui
     gns3-server
-    ubridge    # Required by GNS3 to bridge simulated networks to your physical LAN
+    ubridge # Required by GNS3 to bridge simulated networks to physical LANs
   ];
 }
