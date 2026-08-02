797ee4b7e67ba5d93e011d4d34e3d175bbf9b9c8
Aljam <aljam@live.ca>
2026-08-02 17:11:57 -0400

    Format configuration.nix for consistency

--- hosts/navi/configuration.nix
+++ hosts/navi/configuration.nix
@@ -1,13 +1,13 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-    imports = [
-    	./hardware-configuration.nix
-    	../../modules/roles/desktop-node.nix
-    	../../modules/features/graphics.nix
-        ../../modules/hardware/navi-desktop.nix
-      ];
-    
-    networking.hostName = "navi";
-    programs.corectrl.enable = true; # AMD Overclocking/Undervolting
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/roles/desktop-node.nix
+    ../../modules/features/graphics.nix
+    ../../modules/hardware/navi-desktop.nix
+  ];
+
+  networking.hostName = "navi";
+  programs.corectrl.enable = true; # AMD Overclocking/Undervolting
 }
