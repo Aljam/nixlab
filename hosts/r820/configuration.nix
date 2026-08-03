af94b1a85dd54ef826ef28ebe8f5d76ed59eb5ce
Aljam <aljam@live.ca>
2026-08-03 19:58:16 -0400

    Add mail-node role to configuration

--- hosts/r820/configuration.nix
+++ hosts/r820/configuration.nix
@@ -6,6 +6,7 @@
     ../../modules/hardware/dell-poweredge.nix
     ../../modules/roles/server-core.nix 
     ../../modules/features/libvirt.nix
+    ../../modules/roles/mail-node.nix
   ];
 
   # Bootloader setup for standard non-ZFS partition layout
