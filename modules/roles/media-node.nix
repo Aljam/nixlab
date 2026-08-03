e18d2a23a7eac06ae61f1dd279a1f3e3acc58127
Aljam <aljam@live.ca>
2026-08-03 18:40:38 -0400

    Refactor media-node.nix to use dynamic domains

--- modules/roles/media-node.nix
+++ modules/roles/media-node.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, domains, ... }: # <-- Added 'domains' here
 
 {
   imports = [
@@ -14,30 +14,31 @@
     enable = true;
     openFirewall = true;
     # Binds to localhost to prevent HAProxy Host header validation errors
-    allowedHosts = "home.derezzed.info,127.0.0.1:8082";
+    allowedHosts = "home.${domains.primary}";
+    
     services = [
       {
         "Media & Requests" = [
-          { Jellyfin = { href = "https://jellyfin.derezzed.info"; description = "Media Streaming"; icon = "jellyfin.png"; }; }
-          { Seerr = { href = "https://seerr.derezzed.info"; description = "Media Requests"; icon = "seerr.png"; }; }
-          { Audiobookshelf = { href = "https://audiobookshelf.derezzed.info"; description = "Audiobooks & Podcasts"; icon = "audiobookshelf.png"; }; }
+          { Jellyfin = { href = "https://jellyfin.${domains.primary}"; description = "Media Streaming"; icon = "jellyfin.png"; }; }
+          { Seerr = { href = "https://seerr.${domains.primary}"; description = "Media Requests"; icon = "seerr.png"; }; }
+          { Audiobookshelf = { href = "https://audiobookshelf.${domains.primary}"; description = "Audiobooks & Podcasts"; icon = "audiobookshelf.png"; }; }
         ];
       }
       {
         "Automation & Downloads" = [
-          { Sonarr = { href = "https://sonarr.derezzed.info"; description = "TV Shows"; icon = "sonarr.png"; }; }
-          { Radarr = { href = "https://radarr.derezzed.info"; description = "Movies"; icon = "radarr.png"; }; }
-          { Prowlarr = { href = "https://prowlarr.derezzed.info"; description = "Indexers"; icon = "prowlarr.png"; }; }
-          { Bazarr = { href = "https://bazarr.derezzed.info"; description = "Subtitles Automation"; icon = "bazarr.png"; }; }
-          { Readarr = { href = "https://readarr.derezzed.info"; description = "Books Automation"; icon = "readarr.png"; }; }
-          { Lidarr = { href = "https://lidarr.derezzed.info"; description = "Music Automation"; icon = "lidarr.png"; }; }
-          { qBittorrent = { href = "https://qb.derezzed.info"; description = "Torrents"; icon = "qbittorrent.png"; }; }
-          { Autobrr = { href = "https://autobrr.derezzed.info"; description = "IRC Torrent Filters"; icon = "autobrr.png"; }; }
+          { Sonarr = { href = "https://sonarr.${domains.primary}"; description = "TV Shows"; icon = "sonarr.png"; }; }
+          { Radarr = { href = "https://radarr.${domains.primary}"; description = "Movies"; icon = "radarr.png"; }; }
+          { Prowlarr = { href = "https://prowlarr.${domains.primary}"; description = "Indexers"; icon = "prowlarr.png"; }; }
+          { Bazarr = { href = "https://bazarr.${domains.primary}"; description = "Subtitles Automation"; icon = "bazarr.png"; }; }
+          { Readarr = { href = "https://readarr.${domains.primary}"; description = "Books Automation"; icon = "readarr.png"; }; }
+          { Lidarr = { href = "https://lidarr.${domains.primary}"; description = "Music Automation"; icon = "lidarr.png"; }; }
+          { qBittorrent = { href = "https://qb.${domains.primary}"; description = "Torrents"; icon = "qbittorrent.png"; }; }
+          { Autobrr = { href = "https://autobrr.${domains.primary}"; description = "IRC Torrent Filters"; icon = "autobrr.png"; }; }
         ];
       }
       {
         "System & Monitoring" = [
-          { Grafana = { href = "https://grafana.derezzed.info"; description = "Metrics & Dashboards"; icon = "grafana.png"; }; }
+          { Grafana = { href = "https://grafana.${domains.primary}"; description = "Metrics & Dashboards"; icon = "grafana.png"; }; }
         ];
       }
     ];
