1f03c00f1f9d611cfed7e74feb917c7030176508
Aljam <aljam@live.ca>
2026-08-03 23:31:30 -0400

    Remove graphics feature module from configuration
    Removed graphics feature module from imports.

--- hosts/navi/configuration.nix
+++ hosts/navi/configuration.nix
@@ -4,7 +4,6 @@
   imports = [
     ./hardware-configuration.nix
     ../../modules/roles/desktop-node.nix
-    ../../modules/features/graphics.nix
     ../../modules/hardware/navi-desktop.nix
     ../../modules/features/remote-builder.nix
   ];
