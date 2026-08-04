563c6f3591957fe20deeea24f39b3bd908c4a69e
Aljam <aljam@live.ca>
2026-08-03 22:16:11 -0400

    Enable GRUB boot loader and disable systemd-boot

--- hosts/oryx/configuration.nix
+++ hosts/oryx/configuration.nix
@@ -9,6 +9,12 @@
   ];
 
   networking.hostName = "oryx";
+  
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  # Use "nodev" since grub will install to the EFI system partition without needing to target a specific raw disk block
+  boot.loader.grub.device = "nodev";
 
   system.stateVersion = lib.mkDefault "26.05";
 }
