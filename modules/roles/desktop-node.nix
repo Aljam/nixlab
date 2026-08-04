c8f179d0818c71273a917d1696e3cfe16a6cff4a
Aljam <aljam@live.ca>
2026-08-03 21:19:29 -0400

    Add audio feature import to desktop-node configuration

--- modules/roles/desktop-node.nix
+++ modules/roles/desktop-node.nix
@@ -1,54 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
-  time.timeZone = "America/Toronto";
-  i18n.defaultLocale = "en_CA.UTF-8";
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  security.sudo.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    home-manager
-    git
-    htop
-    wget
-    curl
-    yt-dlp
-    ffmpeg
-    vim
-    cifs-utils
-    kitty.terminfo # Fixes missing terminfo when connecting via SSH from Kitty
-    lm_sensors
-    iotop
-    nvd
-    nix-tree
-    iperf3
-    nmap
-    tcpdump
-    duf
-    sops
-  ];
-
-  services.tailscale.enable = true;
-
-  services.openssh = {
-    enable = true;
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
-    settings.PermitRootLogin = "no";
-  };
-
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-
-  sops = {
-    defaultSopsFile = ../../secrets/secrets.yaml;
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  };
-
-  nix.optimise.automatic = true;
+    imports = [
+      ../features/audio.nix
+    ];
 }
