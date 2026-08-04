957eef4651152097f5cc85a0ad3504fa725e1561
Aljam <aljam@live.ca>
2026-08-04 16:38:29 -0400

    Refactor flake.nix structure and remove whitespace

--- flake.nix
+++ flake.nix
@@ -4,14 +4,14 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
-    
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     mailserver = {
       url = "gitlab:simple-nixos-mailserver/nixos-mailserver/nixos-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -26,96 +26,91 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
     hyprland.url = "github:hyprwm/Hyprland";
   };
 
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, disko, sops-nix, mailserver, ... }@inputs:
-  let
-    system = "x86_64-linux";
-    
-    pkgs-stable = import nixpkgs-stable {
-      inherit system;
-      config.allowUnfree = true;
-    };
+    let
+      system = "x86_64-linux";
 
-    domains = {
-      primary    = "derezzed.info";
-      fuwa       = "fuwa.space";
-      cybal      = "cybal.org";
-      netrunner  = "netrunner.dev";
-      
-      glow_net   = "glowrunner.network";
-      glow_dev   = "glowrunner.dev";
-      glow_xyz   = "glowrunner.xyz";
-    };
+      pkgs-stable = import nixpkgs-stable {
+        inherit system;
+      };
 
-    subnets = {
-      lan = "192.168.1";
-    };
-    
-    mkHost = { hostname, extraModules ? [] }: nixpkgs.lib.nixosSystem {
-      inherit system;
-      
-      specialArgs = { inherit inputs pkgs-stable hostname domains subnets; }; 
-      
-      modules = [
-        ({ ... }: {
-          
-        })
-        ./hosts/${hostname}/configuration.nix
-        ./modules/roles/common.nix
-        ./users/aljam/nixos.nix
-        sops-nix.nixosModules.sops
-        
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.users.aljam = import ./users/aljam/home.nix;
-          home-manager.extraSpecialArgs = { inherit inputs pkgs-stable domains subnets; };
-        }
-      ] ++ extraModules;
-    };
+      domains = {
+        primary    = "derezzed.info";
+        fuwa       = "fuwa.space";
+        cybal      = "cybal.org";
+        netrunner  = "netrunner.dev";
+        glow_net   = "glowrunner.network";
+        glow_dev   = "glowrunner.dev";
+        glow_xyz   = "glowrunner.xyz";
+      };
 
-    desktop = { home-manager.users.aljam.imports = [ ./users/aljam/home-gui.nix ]; };
-    serverDisko = disko.nixosModules.disko;
-    flatpakModule = inputs.nix-flatpak.nixosModules.nix-flatpak;
+      subnets = {
+        lan = "192.168.1";
+      };
 
-  in {
-    nixosConfigurations = {
-      
-      navi = mkHost { 
-        hostname = "navi";
-        extraModules = [ desktop flatpakModule ]; 
+      mkHost = { hostname, extraModules ? [] }: nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        specialArgs = { inherit inputs pkgs-stable hostname domains subnets; };
+
+        modules = [
+          ./hosts/${hostname}/configuration.nix
+          ./modules/roles/common.nix
+          ./users/aljam/nixos.nix
+          sops-nix.nixosModules.sops
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.aljam = import ./users/aljam/home.nix;
+            home-manager.extraSpecialArgs = { inherit inputs pkgs-stable domains subnets; };
+          }
+        ] ++ extraModules;
       };
-      
-      oryx = mkHost { 
-        hostname = "oryx";
-        extraModules = [ 
-          desktop 
-          flatpakModule 
-          inputs.nixos-hardware.nixosModules.system76 
-        ];
+
+      desktop = { home-manager.users.aljam.imports = [ ./users/aljam/home-gui.nix ]; };
+      serverDisko = disko.nixosModules.disko;
+      flatpakModule = inputs.nix-flatpak.nixosModules.nix-flatpak;
+
+    in {
+      nixosConfigurations = {
+
+        navi = mkHost {
+          hostname = "navi";
+          extraModules = [ desktop flatpakModule ];
+        };
+
+        oryx = mkHost {
+          hostname = "oryx";
+          extraModules = [
+            desktop
+            flatpakModule
+            inputs.nixos-hardware.nixosModules.system76
+          ];
+        };
+
+        r820 = mkHost {
+          hostname = "r820";
+        };
+
+        r730 = mkHost {
+          hostname = "r730";
+          extraModules = [ serverDisko ];
+        };
+
+        r730xd = mkHost {
+          hostname = "r730xd";
+          extraModules = [ serverDisko ];
+        };
+
       };
-      
-      r820 = mkHost { 
-        hostname = "r820"; 
-      };
-      
-      r730 = mkHost { 
-        hostname = "r730";
-        extraModules = [ serverDisko ]; 
-      };
-      
-      r730xd = mkHost { 
-        hostname = "r730xd";
-        extraModules = [ serverDisko ];
-      };
-      
     };
-  };
 }
