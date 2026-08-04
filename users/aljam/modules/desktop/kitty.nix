6e6564c91e45cdc0c52530d98e10fbd742e512bc
Aljam <aljam@live.ca>
2026-08-04 17:22:34 -0400

    Add kitty configuration for terminal emulator

---
+++ users/aljam/modules/desktop/kitty.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, pkgs-stable, ... }:
+
+{
+  programs.kitty = {
+    enable = true;
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+    };
+  };
+}
