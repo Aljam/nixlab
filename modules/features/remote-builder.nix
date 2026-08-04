7b26fbad1313673685798b992598de93108fd514
Aljam <aljam@live.ca>
2026-08-04 00:53:23 -0400

    Enable substitutes for faster local builds
    Added optional setting to use substitutes for faster local builds.

--- modules/features/remote-builder.nix
+++ modules/features/remote-builder.nix
@@ -3,18 +3,25 @@
 {
   nix = {
     distributedBuilds = true;
+    
+    # Optional: Don't force builds to the remote if local is faster for tiny packages
+    settings.builders-use-substitutes = true;
+
     buildMachines = [{
-      hostName = "r820"; # Make sure this resolves via Tailscale or local DNS
-      sshUser = "aljam";
-      sshKey = "/root/.ssh/id_ed25519_buildfarm";
+      hostName = "r820"; # Replace with the R820's actual IP (e.g., "192.168.1.XX") if DNS isn't resolving
       system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 32;       # Unleash those quad-socket threads!
-      speedFactor = 3;
+      protocol = "ssh-ng"; # The modern, faster Nix SSH protocol
+      
+      # The user on the R820 that will execute the build
+      sshUser = "aljam"; 
+      
+      # The SSH key that the local nix-daemon (root) will use to connect
+      sshKey = "/root/.ssh/id_ed25519"; 
+      
+      # The R820 is a massive quad-socket server, let it chew through heavy jobs!
+      maxJobs = 32; 
+      speedFactor = 2;
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
     }];
-    extraOptions = ''
-      builders-use-substitutes = true
-    '';
   };
 }
