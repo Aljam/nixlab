8ff819e9faf29a1629a96253e5d4749053062bbe
Aljam <aljam@live.ca>
2026-08-03 20:23:28 -0400

    Add mail-node configuration for mail server setup

---
+++ modules/roles/mail-node.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, domains, lib, ... }:
+
+{
+  sops.secrets."smtp_relay_password" = {
+    owner = "root"; 
+  };
+
+  sops.secrets."mail_password_aljam" = {
+    owner = "root";
+  };
+
+  mailserver = {
+    enable = true;
+    
+    fqdn = "mail.${domains.primary}"; 
+    
+    domains = lib.attrValues domains;
+
+    loginAccounts = {
+      "aljam@${domains.primary}" = {
+        hashedPasswordFile = config.sops.secrets."mail_password_aljam".path;
+        
+        # Dynamically map 'admin@<domain>' for every domain in your fleet,
+        # plus any custom static aliases you want to include!
+        aliases = (map (d: "admin@${d}") (lib.attrValues domains)) ++ [
+          "@${domains.glow_net}" # Wildcard catch-all for glowrunner.network
+        ];
+      };
+    };
+
+    certificateScheme = "acme-nginx";
+
+    # SENDGRID RELAY (Bypasses Bell's Port 25 Block)
+    indexSmarthost = true;
+    relayHosts = [
+      {
+        host = "smtp.sendgrid.net";
+        port = 587; 
+        username = "apikey"; 
+        passwordFile = config.sops.secrets."smtp_relay_password".path;
+      }
+    ];
+  };
+  
+  networking.firewall.allowedTCPPorts = [ 143 465 587 993 ];
+}
