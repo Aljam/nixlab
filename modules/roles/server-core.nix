a0720df8f676b5a4d9f508618f20cb8223a5c5d4
Aljam <aljam@live.ca>
2026-08-04 12:35:44 -0400

    Configure networking with default gateway and nameservers
    Set default gateway and nameservers based on LAN subnets.

--- modules/roles/server-core.nix
+++ modules/roles/server-core.nix
@@ -1,10 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, subnets, ... }:
 
 {
   imports = [
     ../features/sanoid.nix
     ../features/monitoring.nix
   ];
+
+  networking.defaultGateway = "${subnets.lan}.1";
+  networking.nameservers = [ "${subnets.lan}.1" ];
 
   environment.systemPackages = with pkgs; [
     smartmontools
