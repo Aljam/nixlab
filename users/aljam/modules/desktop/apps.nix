00dee7342015bb1198e51566ff02851efd693049
Aljam <aljam@live.ca>
2026-08-04 17:21:05 -0400

    Add desktop applications to Nix configuration

---
+++ users/aljam/modules/desktop/apps.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, pkgs-stable, ... }:
+
+{
+  home.packages = with pkgs; [
+    ungoogled-chromium
+    kdePackages.kate
+    deadbeef-with-plugins
+    discord
+    betterdiscordctl
+    telegram-desktop
+    mpv
+    obsidian
+    librewolf
+    gimp
+    krita
+    blender
+    qbittorrent
+    inkscape
+    audacity
+    super-slicer
+    davinci-resolve
+    element-desktop
+    libreoffice-qt
+    hunspell
+    hunspellDicts.en_US
+    hyphenDicts.en_US
+    pkgs-stable.lutris
+    heroic
+    protonup-qt
+    mangohud
+    corectrl
+    vscodium
+    jetbrains.idea
+    unnamed-sdvx-clone
+    qjackctl
+    # Replaces 'stable' - has the multilib + staging patches for Sound Voltex (EAC)
+    wineWow64Packages.stagingFull
+    winetricks
+    yazi
+    imv
+    pavucontrol
+    pamixer
+    cliphist
+    libnotify
+    reaper
+    audacity
+    lmms
+  ];
+}
