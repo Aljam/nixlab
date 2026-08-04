638d203233ca54f46b56e0f71cb234f1649ea271
Aljam <aljam@live.ca>
2026-08-04 10:18:19 -0400

    Enable Tailscale to accept routes

--- modules/roles/desktop-node.nix
+++ modules/roles/desktop-node.nix
@@ -20,7 +20,7 @@
     enable = true;
     useRoutingFeatures = "client"; # or "both" if advertising routes, but keep local safe
     extraUpFlags = [
-      "--accept-routes=false" # Prevents Tailscale from overriding your local gateway
+      "--accept-routes=true"
     ];
   };
 }
