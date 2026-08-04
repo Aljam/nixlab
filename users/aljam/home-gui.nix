830b7ea5b5af4bc80d38aea894c009809abc16f9
Aljam <aljam@live.ca>
2026-08-04 17:26:31 -0400

    Refactor home-gui.nix to use module imports

--- users/aljam/home-gui.nix
+++ users/aljam/home-gui.nix
@@ -1,74 +1,10 @@
 { config, pkgs, pkgs-stable, ... }:
 
 {
-  xdg.enable = true;
+  imports = [ ./modules/desktop/apps.nix
+              ./modules/desktop/kitty.nix
+              ./modules/desktop/obs.nix
+            ];
 
-  home.packages = with pkgs; [
-    ungoogled-chromium
-    kdePackages.kate
-    deadbeef-with-plugins
-    discord
-    betterdiscordctl
-    telegram-desktop
-    mpv
-    obsidian
-    librewolf
-    gimp
-    krita
-    blender
-    qbittorrent
-    inkscape
-    audacity
-    super-slicer
-    davinci-resolve
-    element-desktop
-    libreoffice-qt
-    hunspell
-    hunspellDicts.en_US
-    hyphenDicts.en_US
-    pkgs-stable.lutris
-    heroic
-    protonup-qt
-    mangohud
-    corectrl
-    vscodium
-    jetbrains.idea
-    unnamed-sdvx-clone
-    qjackctl
-    # Replaces 'stable' - has the multilib + staging patches for Sound Voltex (EAC)
-    wineWow64Packages.stagingFull
-    winetricks
-    yazi
-    imv
-    pavucontrol
-    pamixer
-    cliphist
-    libnotify
-    reaper
-    audacity
-    lmms
-  ];
-
-  programs.kitty = {
-    enable = true;
-    settings = {
-      scrollback_lines = 10000;
-      enable_audio_bell = false;
-      update_check_interval = 0;
-    };
-  };
-
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-backgroundremoval
-      obs-pipewire-audio-capture
-      obs-gstreamer
-      obs-vkcapture
-      input-overlay
-      obs-command-source
-      obs-retro-effects
-    ];
-  };
+ xdg.enable = true;
 }
