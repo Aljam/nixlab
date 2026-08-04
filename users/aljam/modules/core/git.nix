b04ee093edc32bbd3f3a403ee4d4e2f69ecd8884
Aljam <aljam@live.ca>
2026-08-04 17:11:46 -0400

    Configure Git settings for user Aljam

--- users/aljam/modules/core/git.nix
+++ users/aljam/modules/core/git.nix
@@ -1,1 +1,16 @@
+{ config, pkgs, ... }:
 
+{
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Aljam";
+        email = "aljam@live.ca";
+        signingkey = "~/.ssh/id_ed25519.pub";
+      };
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+    };
+  };
+}
