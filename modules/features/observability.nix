74d4484fc06c4a0f41499d39c263f536fc7fd6b7
Aljam <aljam@live.ca>
2026-08-04 09:15:50 +0000

    Fix syntax for defaultSopsFile in observability.nix

--- modules/features/observability.nix
+++ modules/features/observability.nix
@@ -2,7 +2,7 @@
 
 {
   sops.secrets.alertmanager-password = {};
-  sops.defaultSopsFile = ../../../secrets/secrets.yaml
+  sops.defaultSopsFile = ../../../secrets/secrets.yaml;
 
   # Enable and Configure Alertmanager for Email
   services.prometheus.alertmanager = {
