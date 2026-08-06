7868d91afd6352147f81defac3fca81a8ecb462d
Aljam <aljam@live.ca>
2026-08-06 15:49:33 -0400

    Enable EFI variable touch for boot loader

--- hosts/r730xd/configuration.nix
+++ hosts/r730xd/configuration.nix
@@ -27,6 +27,7 @@
   ];
 
   boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
