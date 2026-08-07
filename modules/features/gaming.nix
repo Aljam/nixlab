32cefc7027a062057b48845f07908871fd3c41da
Aljam <aljam@live.ca>
2026-08-06 20:48:27 -0400

    Enhance gaming configuration with additional packages

--- modules/features/gaming.nix
+++ modules/features/gaming.nix
@@ -1,22 +1,43 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
-  programs.gamemode.enable = true;
-  
+  hardware.steam-hardware.enable = true;
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+  };
+  programs.gamemode = {
+    enable = true;
+  };
+  programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
+    package = pkgs.millennium-steam.override {
+      extraPkgs = (pkgs: with pkgs; [
+        gamemode
+        libxi
+        libxcursor
+        libxinerama
+        libxscrnsaver
+        libpulseaudio
+        libvorbis
+        libkrb5
+        keyutils
+        at-spi2-atk
+        libpng
+      ]);
+    };
+    # package = inputs.millennium.packages.x86_64-linux.millennium-steam.override {
+    #   extraPkgs = (pkgs: with pkgs; [
+    #     gamemode
+    #   ]);
+    # };
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
-    protontricks.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
+      nur.repos.vladexa.proton-cachyos
     ];
+    protontricks.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    mangohud
-    protonup-qt
-  ];
 }
