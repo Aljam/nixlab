5db2e42d97c43c12b515079a0d7dbfcf8feed6aa
Aljam <aljam@live.ca>
2026-08-08 18:32:40 -0400

    Modify service configuration in youtube.nix
    Removed ProtectSystem and PrivateTmp settings from service configuration.

--- modules/features/youtube.nix
+++ modules/features/youtube.nix
@@ -32,8 +32,6 @@
       Group = "media";
       StateDirectory = "ytdl-sub";
       ExecStart = "${pkgs.ytdl-sub}/bin/ytdl-sub --config /etc/ytdl-sub/config.yaml sub /etc/ytdl-sub/subscriptions.yaml";
-      ProtectSystem = "strict";
-      PrivateTmp = true;
       ReadWritePaths = [ "/mnt/media/youtube" ];
     };
   };
