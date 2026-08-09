a2885be06968e3dbaa4dfb796f7961de4a590ad2
Aljam <aljam@live.ca>
2026-08-09 11:23:23 -0400

    Disable GRUB and systemd-boot in configuration.nix
    Removed GRUB boot loader configuration.

--- hosts/navi/configuration.nix
+++ hosts/navi/configuration.nix
@@ -12,11 +12,5 @@
 
   networking.networkmanager.enable = true;
 
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.configurationLimit = 10;
-
   system.stateVersion = lib.mkDefault "26.05";
 }
