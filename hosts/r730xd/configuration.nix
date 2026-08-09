5ca63b29d5ce0f9825b262521e60a21c39e27d2d
Aljam <aljam@live.ca>
2026-08-09 11:26:10 -0400

    Disable GRUB bootloader in configuration
    Removed GRUB bootloader configuration settings.

--- hosts/r730xd/configuration.nix
+++ hosts/r730xd/configuration.nix
@@ -26,12 +26,5 @@
     }
   ];
 
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.configurationLimit = 10;
-
   system.stateVersion = lib.mkDefault "26.05";
 }
