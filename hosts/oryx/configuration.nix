90211f147e3ab45e54e479e653d59a4417ab8271
Aljam <aljam@live.ca>
2026-08-09 11:19:04 -0400

    Disable GRUB and enable systemd-boot
    Removed GRUB boot loader configuration settings.

--- hosts/oryx/configuration.nix
+++ hosts/oryx/configuration.nix
@@ -8,12 +8,6 @@
   ];
 
   networking.hostName = "oryx";
-  
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.configurationLimit = 10;
 
   networking.networkmanager.enable = true;
 
