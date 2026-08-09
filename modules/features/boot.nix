1f80b95b439964440169f66badba38d4be441d32
Aljam <aljam@live.ca>
2026-08-09 11:25:18 -0400

    Enable EFI variable touch for systemd-boot

--- modules/features/boot.nix
+++ modules/features/boot.nix
@@ -2,6 +2,7 @@
 
 {
   boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.device = "nodev";
