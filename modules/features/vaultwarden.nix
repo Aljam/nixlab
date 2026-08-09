335a60e964092def069b0355c9282cf36bbb1322
Aljam <aljam@live.ca>
2026-08-09 16:27:44 -0400

    Change ROCKET_ADDRESS to allow external access

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -6,7 +6,7 @@
     backupDir = "/var/backup/vaultwarden";
     config = {
       ROCKET_PORT = 8222;
-      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_ADDRESS = "0.0.0.0";
       SIGNUPS_ALLOWED = false; # SET TO FALSE AFTER ACCOUNT CREATION
       DOMAIN = "https://vault.${domains.primary}";
     };
