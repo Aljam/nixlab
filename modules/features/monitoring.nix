d01e5005e98585a8a5f192d38245962e5dc8380b
Aljam <aljam@live.ca>
2026-08-09 16:41:16 -0400

    Change Grafana HTTP address to listen on all interfaces

--- modules/features/monitoring.nix
+++ modules/features/monitoring.nix
@@ -134,7 +134,7 @@
     enable = true;
     settings = {
       server = {
-        http_addr = "127.0.0.1";
+        http_addr = "0.0.0.0";
         http_port = 3000;
         root_url = "https://grafana.${domains.primary}"; 
         domain = "grafana.${domains.primary}";
