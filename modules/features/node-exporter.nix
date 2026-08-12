8e6c7f22453abccafad5318fed52c8a5ab1a8cc2
Aljam <aljam@live.ca>
2026-08-12 12:09:01 -0400

    Change listen address for node exporter
    Updated the listen address for the node exporter to use extraFlags.

--- modules/features/node-exporter.nix
+++ modules/features/node-exporter.nix
@@ -4,7 +4,6 @@
   services.prometheus.exporters.node = {
     enable = true;
     openFirewall = true;
-    listenAddress = "0.0.0.0";
     port = 9100;
     enabledCollectors = lib.mkForce [
       "systemd"
@@ -16,6 +15,7 @@
       "hwmon"
       "nvme"
     ];
+  extraFlags = [ "--web.listen-address=0.0.0.0:9100" ];
   };
 
   networking.firewall.allowedTCPPorts = [ 9100 ];
