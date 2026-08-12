8fb4247303140891ff23f5791ff91e2d6ba3374a
Aljam <aljam@live.ca>
2026-08-12 06:09:41 -0400

    Set listenAddress for node exporter to 0.0.0.0

--- modules/features/node-exporter.nix
+++ modules/features/node-exporter.nix
@@ -4,6 +4,7 @@
   services.prometheus.exporters.node = {
     enable = true;
     openFirewall = true;
+    listenAddress = "0.0.0.0";
     port = 9100;
     enabledCollectors = lib.mkForce [
       "systemd"
