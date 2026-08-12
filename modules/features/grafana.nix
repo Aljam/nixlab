36daeaa4177b6cc2f4fbfce4f206f309cdb82602
Aljam <aljam@live.ca>
2026-08-12 06:35:58 -0400

    Refactor Grafana configuration and update secrets

--- modules/features/grafana.nix
+++ modules/features/grafana.nix
@@ -1,9 +1,9 @@
 { config, pkgs, domains, ... }:
 
 {
-   sops.secrets.grafana-secret-key = {};
+  sops.secrets."grafana-secret-key".owner = "grafana";
 
-   services.grafana = {
+  services.grafana = {
     enable = true;
     settings = {
       server = {
@@ -11,33 +11,25 @@
         http_port = 3000;
         root_url = "https://grafana.${domains.primary}"; 
         domain = "grafana.${domains.primary}";
-        serve_from_sub_path = true;
       };
-      security = {
-        secret_key = config.sops.secrets.grafana-secret-key.path;
-      };
+      security.secret_key = "$__file{${config.sops.secrets."grafana-secret-key".path}}";
     };
     provision = {
       enable = true;
-      datasources.settings.datasources = [
-        {
-          name = "Prometheus";
-          type = "prometheus";
-          url = "http://127.0.0.1:9090";
-          isDefault = true;
-        }
-      ];
-      dashboards.settings.providers = [
-        {
-          name = "Default";
-          options.path = pkgs.fetchurl {
-            url = "https://grafana.com/api/dashboards/1860/revisions/37/download";
-            sha256 = "sha256-1DE1aaanRHHeCOMWDGdOS1wBXxOF84UXAjJzT5Ek6mM=";
-          };
-        }
-      ];
+      datasources.settings.datasources = [{
+        name = "Prometheus";
+        type = "prometheus";
+        url = "http://127.0.0.1:9090";
+        isDefault = true;
+      }];
+      dashboards.settings.providers = [{
+        name = "Default";
+        options.path = pkgs.fetchurl {
+          url = "https://grafana.com/api/dashboards/1860/revisions/37/download";
+          sha256 = "sha256-1DE1aaanRHHeCOMWDGdOS1wBXxOF84UXAjJzT5Ek6mM=";
+        };
+      }];
     };
   };
-
   networking.firewall.allowedTCPPorts = [ 3000 ];
 }
