c5650049621808d2423e4bfc728ce21b881f960f
Aljam <aljam@live.ca>
2026-08-12 05:30:02 -0400

    Enhance node-exporter configuration
    Added openFirewall option and expanded enabledCollectors.

--- modules/features/node-exporter.nix
+++ modules/features/node-exporter.nix
@@ -1,10 +1,21 @@
 { lib, ... }:
 
-{
   services.prometheus.exporters.node = {
     enable = true;
+    openFirewall = true;
     port = 9100;
-    enabledCollectors = lib.mkForce [ "systemd" "zfs" ];
+    enabledCollectors = lib.mkForce [
+      "systemd"
+      "processes"
+      "cpu"
+      "diskstats"
+      "filesystem"
+      "netdev"
+      "zfs"
+      "hwmon"
+      "nvme"
+    ];
   };
+
   networking.firewall.allowedTCPPorts = [ 9100 ];
 }
