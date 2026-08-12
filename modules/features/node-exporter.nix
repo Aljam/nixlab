d5dbdab488af7f7dc96b37cfb426cf41be93b16f
Aljam <aljam@live.ca>
2026-08-12 12:49:45 -0400

    Remove extraFlags for node-exporter configuration
    Removed extraFlags for web.listen-address in node-exporter.

--- modules/features/node-exporter.nix
+++ modules/features/node-exporter.nix
@@ -16,7 +16,6 @@
       "nvme"
     ];
     firewallFilter = "-i br0 -p tcp -m tcp --dport 9100";
-    extraFlags = [ "--web.listen-address=0.0.0.0:9100" ];
   };
 
   networking.firewall.allowedTCPPorts = [ 9100 ];
