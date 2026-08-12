76d96509eed0f55acaccb4423875cb6d2f97c7ca
Aljam <aljam@live.ca>
2026-08-12 09:49:43 -0400

    Change node exporter listen address to 192.168.1.2

--- modules/features/node-exporter.nix
+++ modules/features/node-exporter.nix
@@ -4,7 +4,7 @@
   services.prometheus.exporters.node = {
     enable = true;
     openFirewall = true;
-    listenAddress = "0.0.0.0";
+    listenAddress = "192.168.1.2";
     port = 9100;
     enabledCollectors = lib.mkForce [
       "systemd"
