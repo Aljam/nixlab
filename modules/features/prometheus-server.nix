f17b4edff7a19f2657d0830d91d31143f628b120
Aljam <aljam@live.ca>
2026-08-12 08:53:14 -0400

    Format Prometheus server targets for consistency

--- modules/features/prometheus-server.nix
+++ modules/features/prometheus-server.nix
@@ -9,9 +9,9 @@
         job_name = "fleet-nodes"; 
         static_configs = [{ 
           targets = [ 
-            "127.0.0.1:9100"        # Scrapes itself (r730xd)
-            "192.168.1.3:9100"      # Replace with the actual IP of r730
-            "192.168.1.4:9100"      # Replace with the actual IP of r820
+            "127.0.0.1:9100"
+            "192.168.1.3:9100"
+            "192.168.1.4:9100"
           ]; 
         }]; 
       }
