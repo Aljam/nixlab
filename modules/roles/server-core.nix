4d96105873d09b9f75a7a8fc7935fcf5160ecdb8
Aljam <aljam@live.ca>
2026-08-13 01:38:42 -0400

    Remove trusted interface from firewall settings
    Remove trusted interface from firewall configuration.

--- modules/roles/server-core.nix
+++ modules/roles/server-core.nix
@@ -9,7 +9,6 @@
   networking.defaultGateway.address = "${subnets.lan}.1";
   networking.nameservers = [ "${subnets.lan}.1" ];
   networking.enableIPv6 = false;
-  networking.firewall.trustedInterfaces = [ "eno1" ];
   networking.firewall.allowPing = true;
 
   networking.networkmanager.enable = false;
