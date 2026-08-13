0520b782d3dda644a39f64e42c598c169899913b
Aljam <aljam@live.ca>
2026-08-13 01:45:22 -0400

    Disable unencrypted connections for WebService

--- hosts/r820/configuration.nix
+++ hosts/r820/configuration.nix
@@ -23,7 +23,7 @@
     port = 9090;
     settings = {
       WebService = {
-        AllowUnencrypted = true; # Safe since it sits behind pfSense/HAProxy
+        AllowUnencrypted = false; # Safe since it sits behind pfSense/HAProxy
       };
     };
   };
