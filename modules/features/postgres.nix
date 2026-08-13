299e4e7a49b56d3abbbed1c87171a17fb3fe29b9
Aljam <aljam@live.ca>
2026-08-13 01:32:58 -0400

    Change pgAdmin to listen on localhost and use subnet
    Updated pgAdmin configuration to use local address and subnet variable.

--- modules/features/postgres.nix
+++ modules/features/postgres.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, subnets, ... }: {
   
   # 1. Define the secret for the pgAdmin initial setup
   # Make sure the pgadmin user owns the secret so it can read it during startup
@@ -18,7 +18,7 @@
       host  all       all     127.0.0.1/32   trust
       host  all       all     ::1/128        trust
       # Allow your entire Tailscale or local subnet to authenticate with a password
-      host  all       all     192.168.1.0/24 md5
+      host  all       all     ${subnets.lan}.0/24 md5
     '';
 
     # Optional: Automatically create a default database and user on first boot
@@ -39,15 +39,11 @@
     initialPasswordFile = config.sops.secrets.pgadmin_password.path;
     port = 5050; # Default port for the web interface
     settings = {
-      DEFAULT_SERVER = "0.0.0.0";
+      DEFAULT_SERVER = "127.0.0.1";
     };
   };
 
   systemd.services.pgadmin.environment = {
-    PGADMIN_LISTEN_ADDRESS = "0.0.0.0";
+    PGADMIN_LISTEN_ADDRESS = "127.0.0.1";
   };
-
-  # 4. Open the Firewall
-  # 5432 = PostgreSQL | 5050 = pgAdmin Web UI
-  networking.firewall.allowedTCPPorts = [ 5432 5050 ];
 }
