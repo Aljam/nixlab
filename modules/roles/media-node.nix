92d25a1f7bb583a1d8700bf886f3d43162b47128
Aljam <aljam@live.ca>
2026-08-13 03:18:16 -0400

    Update allowedHosts for homepage-dashboard service
    Removed '127.0.0.1:8082' from allowedHosts for homepage-dashboard.

--- modules/roles/media-node.nix
+++ modules/roles/media-node.nix
@@ -14,7 +14,7 @@
   services.homepage-dashboard = {
     enable = true;
     # Binds to localhost to prevent HAProxy Host header validation errors
-    allowedHosts = "home.${domains.primary},127.0.0.1:8082";
+    allowedHosts = "home.${domains.primary}";
     
     services = [
       {
