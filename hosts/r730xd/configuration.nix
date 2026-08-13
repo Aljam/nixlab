520d3747b1735d26a3d8d3324467c29358233a59
Aljam <aljam@live.ca>
2026-08-13 05:39:19 -0400

    Add youtube-cookie-sync module to configuration

--- hosts/r730xd/configuration.nix
+++ hosts/r730xd/configuration.nix
@@ -8,6 +8,7 @@
     ../../modules/roles/server-core.nix
     ../../modules/features/nvidia-headless.nix
     ../../modules/roles/media-node.nix
+    ../../modules/features/youtube-cookie-sync.nix
     ../../modules/roles/storage-node.nix
     ../../modules/features/prometheus-server.nix
   ];
