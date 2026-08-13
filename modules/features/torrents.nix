72571d1896d29785e4056d91e5fbfdfd16f36f61
Aljam <aljam@live.ca>
2026-08-13 02:17:39 -0400

    Refactor qbitmanage container configuration
    Removed qbitmanageImage definition and its usage.

--- modules/features/torrents.nix
+++ modules/features/torrents.nix
@@ -8,19 +8,8 @@
     webuiPort = 8080;
   };
 
-  let
-    qbitmanageImage = pkgs.dockerTools.pullImage {
-      imageName = "ghcr.io/starbix/qbitmanage";
-      imageDigest = "sha256:....";  # registry digest
-      sha256 = "sha256-....";       # Nix nar hash from nix-prefetch-docker
-      finalImageName = "ghcr.io/starbix/qbitmanage";
-      finalImageTag = "v4.11.0";
-    };
-  in
-  {
     virtualisation.oci-containers.containers.qbitmanage = {
       image = "ghcr.io/starbix/qbitmanage:v4.11.0";
-      imageFile = qbitmanageImage;
       environment = {
         QBT_RUN = "true";
         QBT_SCHEDULE = "1440";
