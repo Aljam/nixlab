53b02410a7dbf4008f42f43d224a47b24b29b468
Aljam <aljam@live.ca>
2026-08-13 02:15:39 -0400

    Refactor qbitmanage container configuration

--- modules/features/torrents.nix
+++ modules/features/torrents.nix
@@ -8,16 +8,28 @@
     webuiPort = 8080;
   };
 
-  virtualisation.oci-containers.containers.qbitmanage = {
-    image = "ghcr.io/starbix/qbitmanage:latest";
-    environment = {
-      QBT_RUN = "true";
-      QBT_SCHEDULE = "1440";
+  let
+    qbitmanageImage = pkgs.dockerTools.pullImage {
+      imageName = "ghcr.io/starbix/qbitmanage";
+      imageDigest = "sha256:....";  # registry digest
+      sha256 = "sha256-....";       # Nix nar hash from nix-prefetch-docker
+      finalImageName = "ghcr.io/starbix/qbitmanage";
+      finalImageTag = "v4.11.0";
     };
-    volumes = [
-      "/var/lib/qbitmanage:/config"
-      "/mnt/media:/data/media"
-      "/var/lib/qbittorrent:/qbittorrent"
-    ];
+  in
+  {
+    virtualisation.oci-containers.containers.qbitmanage = {
+      image = "ghcr.io/starbix/qbitmanage:v4.11.0";
+      imageFile = qbitmanageImage;
+      environment = {
+        QBT_RUN = "true";
+        QBT_SCHEDULE = "1440";
+      };
+      volumes = [
+        "/var/lib/qbitmanage:/config"
+        "/mnt/media:/data/media"
+        "/var/lib/qbittorrent:/qbittorrent"
+      ];
+    };
   };
 }
