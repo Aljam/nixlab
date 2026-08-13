06659aa16a54775b67a89a7fd82aea82b97bbc4b
Aljam <aljam@live.ca>
2026-08-13 01:20:25 -0400

    Clean up pytest workaround in monitoring.nix
    Removed workaround for upstream pytest failures in monitoring.nix.

--- modules/features/monitoring.nix
+++ modules/features/monitoring.nix
@@ -5,14 +5,6 @@
    
    environment.systemPackages = with pkgs; [
     btop
-    
-    # Workaround for upstream pytest failures
-    (glances.overrideAttrs (oldAttrs: {
-      doCheck = false;
-      dontCheck = true;
-      pytestCheckPhase = "true";
-    }))
-
     iotop
     sysstat
   ];
