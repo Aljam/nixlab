1c432234b8188ec85a6088d9ec8caa4a3949d899
Aljam <aljam@live.ca>
2026-08-13 06:58:31 -0400

    Change ytdl-sub timer from every 5 minutes to hourly

--- modules/features/youtube.nix
+++ modules/features/youtube.nix
@@ -52,7 +52,7 @@
     description = "Timer for ytdl-sub YouTube automation";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "*:0/5";
+      OnCalendar = "*:hourly";
       Persistent = true;
     };
   };
