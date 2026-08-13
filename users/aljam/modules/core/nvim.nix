4be4aac35ec9cf6374a5fa626fb8a726b0f16b8c
Aljam <aljam@live.ca>
2026-08-13 11:35:12 -0400

    Add development packages for Neovim setup
    Added essential packages for development and utilities.

--- users/aljam/modules/core/nvim.nix
+++ users/aljam/modules/core/nvim.nix
@@ -7,4 +7,23 @@
     viAlias = true;
     vimAlias = true;
   };
+
+  home.packages = with pkgs; [
+    # Search and file finding (Required for Telescope)
+    ripgrep
+    fd
+
+    # C Compiler (Required for nvim-treesitter to compile syntax highlighting)
+    gcc 
+    gnumake
+
+    # Utilities
+    unzip
+    wget
+    curl
+
+    # Clipboard support (Wayland and X11)
+    wl-clipboard
+    xclip
+  ];
 }
