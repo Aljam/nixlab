1d5816d081b8f54ab11a66a24ae7e7c6e74f0f50
Aljam <aljam@live.ca>
2026-08-13 01:39:41 -0400

    Update ROCKET_ADDRESS to '127.0.0.1'
    Change ROCKET_ADDRESS to bind to localhost.

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -6,7 +6,7 @@
     backupDir = "/var/backup/vaultwarden";
     config = {
       ROCKET_PORT = 8222;
-      ROCKET_ADDRESS = "0.0.0.0";
+      ROCKET_ADDRESS = "127.0.0.1";
       SIGNUPS_ALLOWED = false; # SET TO FALSE AFTER ACCOUNT CREATION
       DOMAIN = "https://vault.${domains.primary}";
     };
