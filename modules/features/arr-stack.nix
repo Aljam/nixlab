fab5a5c3e1401a2b40d43d4737edbef0b307b426
Aljam <aljam@live.ca>
2026-08-13 03:59:39 -0400

    Fix formatting in arr-stack.nix for mediaUsers

--- modules/features/arr-stack.nix
+++ modules/features/arr-stack.nix
@@ -6,7 +6,7 @@ let
     group = "media";
   } // extra;
 
-  mediaUsers = [ "autobrr" "sonarr" "radarr" "prowlarr" "bazarr" "readarr" "lidarr" "shoko" ];
+  mediaUsers = [ "autobrr" "sonarr" "radarr" "bazarr" "readarr" "lidarr" "shoko" ];
 in
 {
   # SOPS secret declarations
