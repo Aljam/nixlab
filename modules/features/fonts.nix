498cd7e7546b0d6dbb68ebca528dd9f9f465f05f
Aljam <aljam@live.ca>
2026-08-13 10:21:00 -0400

    Create font configuration in fonts.nix
    Add font configuration for system with default and coding fonts.

---
+++ modules/features/fonts.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Font configuration for the system
+  fonts = {
+    # Enable a basic set of fonts providing several font styles and families and reasonable coverage of Unicode.
+    enableDefaultPackages = true;
+
+    # Modern NixOS (23.11+) uses `fonts.packages` instead of `fonts.fonts`
+    packages = with pkgs; [
+      # Standard essential fonts
+      noto-fonts
+      noto-fonts-cjk-sans # Chinese, Japanese, Korean
+      noto-fonts-emoji
+      liberation_ttf
+      corefonts # Microsoft Arial, Times New Roman, etc.
+      ubuntu_font_family
+
+      # Coding & Terminal Fonts (Nerd Fonts)
+      # In newer NixOS versions, you cherry-pick specific Nerd Fonts to save space
+      (nerdfonts.override { 
+        fonts = [ 
+          "FiraCode" 
+          "Meslo" 
+          "JetBrainsMono" 
+          "Hack" 
+          "CascadiaCode"
+        ]; 
+      })
+    ];
+
+    # System-wide default font configurations
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif" "Liberation Serif" ];
+        sansSerif = [ "Noto Sans" "Ubuntu" "Liberation Sans" ];
+        monospace = [ "JetBrainsMono Nerd Font" "FiraCode Nerd Font" ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
+}
