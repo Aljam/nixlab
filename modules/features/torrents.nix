145df74eee1c8e12d05ccad9d290bf8a5a8186fb
Aljam <aljam@live.ca>
2026-08-13 02:24:53 -0400

    Refactor qbitmanage container configuration

--- modules/features/torrents.nix
+++ modules/features/torrents.nix
@@ -8,17 +8,16 @@
     webuiPort = 8080;
   };
 
-    virtualisation.oci-containers.containers.qbitmanage = {
-      image = "ghcr.io/starbix/qbitmanage:v4.11.0";
-      environment = {
-        QBT_RUN = "true";
-        QBT_SCHEDULE = "1440";
-      };
-      volumes = [
-        "/var/lib/qbitmanage:/config"
-        "/mnt/media:/data/media"
-        "/var/lib/qbittorrent:/qbittorrent"
-      ];
+  virtualisation.oci-containers.containers.qbitmanage = {
+    image = "ghcr.io/starbix/qbitmanage:v4.11.0";
+    environment = {
+      QBT_RUN = "true";
+      QBT_SCHEDULE = "1440";
     };
+    volumes = [
+      "/var/lib/qbitmanage:/config"
+      "/mnt/media:/data/media"
+      "/var/lib/qbittorrent:/qbittorrent"
+    ];
   };
 }
