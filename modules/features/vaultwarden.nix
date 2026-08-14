8411e1c63152881f586ae96f3773a6dd00c2740a
Aljam <aljam@live.ca>
2026-08-14 14:55:04 -0400

    Update ROCKET_ADDRESS to use fleet IP

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, domains, subnets, ... }:
+{ config, pkgs, domains, fleet, ... }:
 
 {
   services.vaultwarden = {
@@ -6,7 +6,7 @@
     backupDir = "/var/backup/vaultwarden";
     config = {
       ROCKET_PORT = 8222;
-      ROCKET_ADDRESS = "${subnets.lan}.2";
+      ROCKET_ADDRESS = "${fleet.r730xd.ip}";
       SIGNUPS_ALLOWED = false; # SET TO FALSE AFTER ACCOUNT CREATION
       DOMAIN = "https://vault.${domains.primary}";
     };
