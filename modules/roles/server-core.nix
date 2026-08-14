3a3f556ea6ff82508a8504396c4b42ea37759893
Aljam <aljam@live.ca>
2026-08-14 17:58:28 -0400

    Switch from Docker to Podman for virtualization

--- modules/roles/server-core.nix
+++ modules/roles/server-core.nix
@@ -12,7 +12,7 @@
   networking.nftables.enable = true;
   networking.networkmanager.enable = false;
   networking.useDHCP = false;
-  virtualisation.docker.enable = true;
+  virtualisation.podman = { enable = true; dockerSocket.enable = true; };
   
   environment.systemPackages = with pkgs; [
     smartmontools
