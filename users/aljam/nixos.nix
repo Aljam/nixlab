8be411ad51fe64f43083f45e610c236607017cbb
Aljam <aljam@live.ca>
2026-08-14 18:00:26 -0400

    Update user extraGroups in nixos.nix
    Removed 'plugdev' group from extraGroups.

--- users/aljam/nixos.nix
+++ users/aljam/nixos.nix
@@ -11,7 +11,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHzrMiro1XD8krk5Kb4EWQ+rGjmgKXha/OuOmUZcopRL navi-desktop"
     ];
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "wireshark" "ubridge" "video" "render" "media" "input" "plugdev" ];
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker" "wireshark" "video" "render" "media" "input" ];
     shell = pkgs.fish; # Or whichever shell you use
     hashedPasswordFile = config.sops.secrets.aljam_password.path;
   };
