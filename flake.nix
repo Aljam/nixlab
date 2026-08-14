abf16e3a941464da804df94d3d8c6413e81facd7
Aljam <aljam@live.ca>
2026-08-14 15:19:25 -0400

    Configure Nix substituters and trusted keys
    Added Nix settings for substituters and trusted keys.

--- flake.nix
+++ flake.nix
@@ -42,6 +42,18 @@
     };
   };
 
+  nix.settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org" # see §32
+    ];
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs = {
     self,
     nixpkgs,
