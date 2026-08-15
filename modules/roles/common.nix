2dd4fd5380e83ab54557cc7e512a6870eb562ff4
Aljam <aljam@live.ca>
2026-08-15 12:06:48 -0400

    Add additional Nix substituters and trusted keys

--- modules/roles/common.nix
+++ modules/roles/common.nix
@@ -17,11 +17,14 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org"
+      "https://aljam.cachix.org"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
+      "https://cuda-maintainers.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "aljam.cachix.org-1:E3E80fk8YEaTw0Y9V+0IHmhrvLQ/xACZ1VMXDZZ80oo="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
