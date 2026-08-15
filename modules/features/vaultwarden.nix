3102d6ded5c2f62bc21d9f857c34fec34e2d3a92
Aljam <aljam@live.ca>
2026-08-15 16:18:41 -0400

    fix: remove openFirewall from vaultwarden

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -17,7 +17,6 @@ in
         ROCKET_ADDRESS = "0.0.0.0";
         ROCKET_PORT = 8000;
       };
-      openFirewall = false;
     };
     networking.firewall.extraInputRules = ''
       ip saddr ${subnets.lan}.1 tcp dport 8000 accept
