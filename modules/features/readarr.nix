e03efb2d347a1d08b7576eccbc0804724391533f
Aljam <aljam@live.ca>
2026-08-15 17:22:08 -0400

    Refactor firewall rules for readarr accessibility

--- modules/features/readarr.nix
+++ modules/features/readarr.nix
@@ -9,9 +9,8 @@
     settings.server.bindAddress = "0.0.0.0";
   };
 
-    # Firewall: readarr accessible only from HAProxy (192.168.1.1)
-    networking.firewall.extraInputRules = ''
-      ip saddr 192.168.1.1 tcp dport 8787 accept
-    '';
-  };
+  # Firewall: readarr accessible only from HAProxy (192.168.1.1)
+  networking.firewall.extraInputRules = ''
+    ip saddr 192.168.1.1 tcp dport 8787 accept
+  '';
 }
