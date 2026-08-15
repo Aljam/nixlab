175a9fe9e8681ec42425575058fafbf03153fab8
Aljam <aljam@live.ca>
2026-08-15 14:15:47 -0400

    fix: remove non-existent dataDir option from shoko

--- modules/features/shoko.nix
+++ modules/features/shoko.nix
@@ -1,13 +1,19 @@
-{ config, pkgs, domains, fleet, ... }:
+# nixlab/modules/features/shoko.nix
+# Shoko Server - Anime metadata and library management
+
+{ config, lib, pkgs, ... }:
+
 {
-  services.shoko = {
-    enable = true;
-    user = "media";
-    group = "media";
-    dataDir = "/var/lib/shoko";
-    openFirewall = false;
+  options.modules.features.shoko = {
+    enable = lib.mkEnableOption "Shoko Server anime library management";
   };
-  networking.firewall.extraInputRules = ''
-    ip saddr ${fleet.r730xd.ip} tcp dport 8111 accept
-  '';
+
+  config = lib.mkIf config.modules.features.shoko.enable {
+    services.shoko = {
+      enable = true;
+    };
+
+    # Firewall: shoko accessible only from HAProxy
+    networking.firewall.allowedTCPPorts = [ 8111 ];
+  };
 }
