4cca6429559bdae7ec80b4c794d3236cc98ef54b
Aljam <aljam@live.ca>
2026-08-15 18:20:59 -0400

    Refactor Vaultwarden service configuration

--- modules/features/vaultwarden.nix
+++ modules/features/vaultwarden.nix
@@ -1,25 +1,14 @@
 { config, pkgs, domains, fleet, subnets, lib, ... }:
 
-let
-  cfg = config.modules.features.vaultwarden;
-in
 {
-  options.modules.features.vaultwarden = {
-    enable = lib.mkEnableOption "Vaultwarden password manager" // {
-      default = true;
+  services.vaultwarden = {
+    enable = true;
+    config = {
+      ROCKET_ADDRESS = "0.0.0.0";
+      ROCKET_PORT = 8000;
     };
   };
-
-  config = lib.mkIf cfg.enable {
-    services.vaultwarden = {
-      enable = true;
-      config = {
-        ROCKET_ADDRESS = "0.0.0.0";
-        ROCKET_PORT = 8000;
-      };
-    };
-    networking.firewall.extraInputRules = ''
-      ip saddr ${subnets.lan}.1 tcp dport 8000 accept
-    '';
-  };
+  networking.firewall.extraInputRules = ''
+    ip saddr ${subnets.lan}.1 tcp dport 8000 accept
+  '';
 }
