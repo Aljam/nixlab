5e33e118949bc663806d672be698d3243c66040c
Aljam <aljam@live.ca>
2026-08-15 14:40:46 -0400

    Update firewall rules for Readarr service

--- modules/features/readarr.nix
+++ modules/features/readarr.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, domains, fleet, ... }:
+{ config, pkgs, domains, fleet, subnets, ... }:
 {
   services.readarr = {
     enable = true;
@@ -8,6 +8,6 @@
     openFirewall = false;
   };
   networking.firewall.extraInputRules = ''
-    ip saddr ${fleet.r730xd.ip} tcp dport 8787 accept
+    ip saddr ${subnets.lan}.1 tcp dport 8787 accept
   '';
 }
