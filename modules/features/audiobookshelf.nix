6c033647cfe355c00f7d20797afbd2989928f9b7
Aljam <aljam@live.ca>
2026-08-15 15:33:49 -0400

    fix: add systemd override for audiobookshelf WorkingDirectory

--- modules/features/audiobookshelf.nix
+++ modules/features/audiobookshelf.nix
@@ -7,6 +7,12 @@
     dataDir = "/var/lib/audiobookshelf";
     openFirewall = false;
   };
+
+  # Override broken WorkingDirectory in audiobookshelf systemd service
+  systemd.services.audiobookshelf = {
+    serviceConfig.WorkingDirectory = "/var/lib/audiobookshelf";
+  };
+
   networking.firewall.extraInputRules = ''
     ip saddr ${subnets.lan}.1 tcp dport 13378 accept
   '';
