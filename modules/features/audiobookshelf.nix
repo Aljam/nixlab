c2e3a9020e4fc36c0388f54666298945c9dc75a0
Aljam <aljam@live.ca>
2026-08-15 15:32:33 -0400

    fix: remove WorkingDirectory override from audiobookshelf

--- modules/features/audiobookshelf.nix
+++ modules/features/audiobookshelf.nix
@@ -6,9 +6,6 @@
     group = "media";
     dataDir = "/var/lib/audiobookshelf";
     openFirewall = false;
-    serviceOverrides = {
-      Service.WorkingDirectory = "/var/lib/audiobookshelf";
-    };
   };
   networking.firewall.extraInputRules = ''
     ip saddr ${subnets.lan}.1 tcp dport 13378 accept
