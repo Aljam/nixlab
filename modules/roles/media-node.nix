8f0c727aab9c86fa4f9d5b1aa3baca806e319ffa
Aljam <aljam@live.ca>
2026-08-15 16:23:06 -0400

    restore media-node.nix with vaultwarden and grafana

--- modules/roles/media-node.nix
+++ modules/roles/media-node.nix
@@ -12,7 +12,39 @@
     ../features/shoko.nix
     ../features/torrents.nix
     ../features/jellyfin.nix
+    ../features/recyclarr.nix
+    ../features/prowlarr.nix
     ../features/vaultwarden.nix
     ../features/grafana.nix
+    #../features/nas-mount.nix
+  ];
+
+  # Media user and group
+  users.users.media = {
+    description = "Media services user";
+    group = "media";
+    home = "/var/lib/media";
+    createHome = true;
+    isSystemUser = true;
+  };
+
+  users.groups.media = {};
+
+  # Common media directories
+  systemd.tmpfiles.rules = [
+    "d /var/lib/media 0755 media media -"
+    "d /var/lib/radarr 0755 media media -"
+    "d /var/lib/sonarr 0755 media media -"
+    "d /var/lib/lidarr 0755 media media -"
+    "d /var/lib/readarr 0755 media media -"
+    "d /var/lib/bazarr 0755 media media -"
+    "d /var/lib/seerr 0755 media media -"
+    "d /var/lib/audiobookshelf 0755 media media -"
+    "d /var/lib/autobrr 0755 media media -"
+    "d /var/lib/shoko 0755 media media -"
+    "d /var/lib/jellyfin 0755 media media -"
+    "d /var/lib/torrents 0755 media media -"
+    "d /var/lib/vaultwarden 0755 media media -"
+    "d /var/lib/grafana 0755 media media -"
   ];
 }
