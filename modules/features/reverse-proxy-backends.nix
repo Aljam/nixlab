a96f928a297c7057c1a31e43742c7cc32dce8274
Aljam <aljam@live.ca>
2026-08-15 14:12:51 -0400

    fix: correct set comprehension syntax in firewall rule

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -1,57 +1,36 @@
-{ lib, subnets, ... }:
+# nixlab/modules/features/reverse-proxy-backends.nix
+# Reverse proxy backends configuration with firewall isolation
 
-let
-  # HAProxy runs on the pfSense gateway
-  proxy = "${subnets.lan}.1";
-  
-  # LAN subnet for admin access
-  lanSubnet = "${subnets.lan}.0/24";
+{ config, lib, pkgs, ... }:
 
-  # Public-facing services (accessible via HAProxy)
-  publicBackendPorts = [
-    8096   # jellyfin
-    8989   # sonarr
-    7878   # radarr
-    9696   # prowlarr
-    6767   # bazarr
-    8686   # lidarr
-    8787   # readarr
-    8111   # shoko
-    5055   # jellyseerr
-    13378  # audiobookshelf
-    7474   # autobrr
-    8222   # vaultwarden
-    3000   # grafana
-    8082   # homepage-dashboard
-  ];
-  
-  # Sensitive services (HAProxy only, no LAN access)
-  sensitivePorts = [
-    8222   # vaultwarden - password manager
-    3000   # grafana - monitoring dashboards
-  ];
-  
-  # qBittorrent: WebUI should NEVER be publicly accessible
-  # Only allow HAProxy to reach it (for Jackett/Prowlarr integration)
-  qbittorrentPort = 8080;
-in
 {
-  # Default firewall policy: deny all incoming (set in server-core.nix)
-  
-  networking.firewall.extraInputRules = ''
-    # Allow HAProxy (pfSense gateway) to reach all backend services
-    ip saddr ${proxy} tcp dport { ${lib.concatMapStringsSep ", " toString publicBackendPorts} } accept
-    
-    # Allow LAN subnet to access non-sensitive services for direct admin access
-    # (optional - remove if you only want HAProxy access)
-    ip saddr ${lanSubnet} tcp dport { 
-      ${lib.concatMapStringsSep ", " toString (lib.filter (p: !(p ∈ sensitivePorts)) publicBackendPorts)} 
-    } accept
-    
-    # qBittorrent: ONLY HAProxy can access (never expose to LAN directly)
-    ip saddr ${proxy} tcp dport ${toString qbittorrentPort} accept
-  '';
-  
-  # Log rejected packets for security auditing
-  networking.firewall.logReject = true;
+  options.modules.features.reverse-proxy-backends = {
+    enable = lib.mkEnableOption "reverse proxy backends firewall rules";
+    lanSubnet = lib.mkOption {
+      type = lib.types.str;
+      default = "192.168.1.0/24";
+      description = "LAN subnet for firewall rules";
+    };
+    publicBackendPorts = lib.mkOption {
+      type = lib.types.listOf lib.types.port;
+      default = [ 80 443 ];
+      description = "Public backend ports";
+    };
+    sensitivePorts = lib.mkOption {
+      type = lib.types.listOf lib.types.port;
+      default = [ 22 3306 5432 ];
+      description = "Sensitive ports to exclude from public access";
+    };
+  };
+
+  config = lib.mkIf config.modules.features.reverse-proxy-backends.enable {
+    networking.firewall = {
+      extraCommands = lib.mkBefore ''
+        # Allow LAN subnet to access public backend ports (excluding sensitive ports)
+        ip saddr ${config.modules.features.reverse-proxy-backends.lanSubnet} tcp dport {
+          ${lib.concatMapStringsSep ", " toString (lib.filter (p: !(lib.elem p config.modules.features.reverse-proxy-backends.sensitivePorts)) config.modules.features.reverse-proxy-backends.publicBackendPorts)}
+        } accept
+      '';
+    };
+  };
 }
