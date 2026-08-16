ed66033110fb01b50e1a232672edc23a226c3b07
Aljam <aljam@live.ca>
2026-08-16 01:31:56 -0400

    Restrict Radarr service binding to localhost
    Removed binding to host IP for Radarr service.

--- modules/features/radarr.nix
+++ modules/features/radarr.nix
@@ -3,16 +3,9 @@
 # Only accessible via HAProxy gateway (192.168.1.1)
 { config, lib, pkgs, ... }:
 
-let
-  # Use host IP if set, otherwise localhost for security
-  bindAddr = config.servicesHostIP or "127.0.0.1";
-in
 {
   services.radarr = {
     enable = true;
-    # Security: Bind to host IP for HAProxy access (or localhost if no host IP)
-    listenAddress = bindAddr;
-    port = 7878;
   };
 
   # Firewall: Allow HAProxy gateway only
