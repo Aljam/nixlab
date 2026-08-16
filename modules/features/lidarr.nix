7b3a302d20f77bf2b63fb632410d326772e2862d
Aljam <aljam@live.ca>
2026-08-16 01:27:08 -0400

    Remove bindAddress and listenPort for Lidarr
    Removed security settings for Lidarr service.

--- modules/features/lidarr.nix
+++ modules/features/lidarr.nix
@@ -5,9 +5,6 @@
 {
   services.lidarr = {
     enable = true;
-    # Security: Bind to localhost only (not 0.0.0.0)
-    listenPort = 8686;
-    bindAddress = "127.0.0.1";
   };
 
   # Firewall: Handled centrally by reverse-proxy-backends.nix
