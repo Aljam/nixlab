a0a2f7edc22abe50dc77ceee8678d04e6059915e
Aljam <aljam@live.ca>
2026-08-16 01:53:11 -0400

    Remove domain, subnet, and fleet configurations
    Removed domain, subnet, and fleet configurations from flake.nix.

--- flake.nix
+++ flake.nix
@@ -63,30 +63,6 @@
         config.allowUnfree = true;
       };
 
-      domains = {
-        primary    = "derezzed.info";
-        fuwa       = "fuwa.space";
-        cybal      = "cybal.org";
-        netrunner  = "netrunner.dev";
-        glow_net   = "glowrunner.network";
-        glow_dev   = "glowrunner.dev";
-        glow_xyz   = "glowrunner.xyz";
-      };
-
-      subnets = {
-        lan = "192.168.1";
-        management = [ "127.0.0.0/8" ];
-      };
-
-      fleet = {
-        navi = { };
-        oryx = { };
-        r820 = { ip = "${subnets.lan}.4"; };
-        r730 = { ip = "${subnets.lan}.3"; zpool = "r730pool"; };
-        r730xd = { ip = "${subnets.lan}.2"; zpool = "mediapool"; };
-        proxy = { ip = "${subnets.lan}.1"; };
-      };
-
       mkHost = { hostname, extraModules ? [] }: nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs pkgs-stable hostname domains subnets fleet; };
