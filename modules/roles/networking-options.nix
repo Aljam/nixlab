4e65252801554e3a971626f626c682a5da713a03
Aljam <aljam@live.ca>
2026-08-16 02:00:40 -0400

    Refactor networking options with IPs and domains
    Updated networking options with specific IP assignments and subnet definitions. Added multiple domain options for the host.

--- modules/roles/networking-options.nix
+++ modules/roles/networking-options.nix
@@ -1,24 +1,36 @@
 # modules/roles/networking-options.nix
 # Custom networking options for fleet management
-{ lib, domains, ... }:
+{ lib, ... }:
 {
   options.networking = {
-    fleet = lib.mkOption {
-      type = lib.types.attrs;
-      default = {};
-      description = "Fleet configuration for proxy and hosts";
+    fleet = {
+      navi = { };
+      oryx = { };
+      r820 = { ip = "${subnets.lan}.4"; };
+      r730 = { ip = "${subnets.lan}.3"; zpool = "r730pool"; };
+      r730xd = { ip = "${subnets.lan}.2"; zpool = "mediapool"; };
+      proxy = { ip = "${subnets.lan}.1"; };
     };
 
     subnets = lib.mkOption {
       type = lib.types.attrs;
-      default = {};
+      default = "192.168.1.0/24";
+      lan = "192.168.1";
+      management = "127.0.0.0/8";
       description = "Subnet definitions for the fleet";
     };
 
     domain = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = domains.primary;
+      type = lib.types.null Or lib.types.str;
+      default = domain.derezzed;
       description = "Primary domain for the host";
+      derezzed    = "derezzed.info";
+      fuwa       = "fuwa.space";
+      cybal      = "cybal.org";
+      netrunner  = "netrunner.dev";
+      glow_net   = "glowrunner.network";
+      glow_dev   = "glowrunner.dev";
+      glow_xyz   = "glowrunner.xyz";
     };
   };
 }
