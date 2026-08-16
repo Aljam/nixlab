636064bbaa17dd89570c0cc5d5f3a3213f3e9fae
Aljam <aljam@live.ca>
2026-08-15 22:50:41 -0400

    fix(reverse-proxy-backends.nix): add Homepage-dashboard port 8082
    - Add 8082 (Homepage-dashboard) to the port set
    - Full port set: 3000 (Grafana), 5050 (pgAdmin), 5055 (Seerr), 8082 (Homepage-dashboard), 9090 (Prometheus), 9093 (Alertmanager), 9100 (node-exporter), 13378 (Audiobookshelf), 8222 (Vaultwarden), 8989 (Sonarr), 8686 (Lidarr), 8787 (Readarr), 7878 (Radarr), 9696 (Prowlarr), 8080/8081 (qBittorrent), 8096 (Jellyfin)

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -16,7 +16,7 @@ in
     # nftables rules to allow proxy IP to reach backend services
     extraInputRules = ''
       # Allow proxy IP to reach backend services
-      ip saddr ${proxyIP} tcp dport { 3000, 5050, 5055, 9090, 9093, 9100, 13378, 8222, 8989, 8686, 8787, 7878, 9696, 8080, 8081, 8096 } accept comment "HAProxy backend access"
+      ip saddr ${proxyIP} tcp dport { 3000, 5050, 5055, 8082, 9090, 9093, 9100, 13378, 8222, 8989, 8686, 8787, 7878, 9696, 8080, 8081, 8096 } accept comment "HAProxy backend access"
     '';
   };
 }
