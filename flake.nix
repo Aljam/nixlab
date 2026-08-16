cb4958989b26f43775a95ca28e4f68dff2229d65
Aljam <aljam@live.ca>
2026-08-15 20:57:57 -0400

    revert(flake.nix): restore original structure
    - Remove networking config that was added
    - Restore original imports and structure
    - Revert to original SHA 758b27a

--- flake.nix
+++ flake.nix
@@ -5,7 +5,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # NixOS modules
     sops-nix.url = "github:mic92/sops-nix";
+
+    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,37 +18,34 @@
 
   outputs = { self, nixpkgs, sops-nix, home-manager }:
     let
-      # Network configuration - single source of truth
-      networking = {
-        fleet = {
-          proxy.ip = "192.168.1.1";
-        };
-        subnets = {
-          lan = "192.168.1.0/24";
-          management = [ "127.0.0.0/8" ];
-        };
-      };
-
+      # Supported systems
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+
+      # Helper function to generate attribute sets for each supported system
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Import NixOS configurations
       inherit (import ./hosts) navi oryx r730 r730xd r820;
+
+      # Import user configurations
       inherit (import ./users) aljam;
     in
     {
-      networking = networking;
-
+      # NixOS configurations
       nixosConfigurations = {
-        navi = navi { inherit nixpkgs self sops-nix home-manager networking; };
-        oryx = oryx { inherit nixpkgs self sops-nix home-manager networking; };
-        r730 = r730 { inherit nixpkgs self sops-nix home-manager networking; };
-        r730xd = r730xd { inherit nixpkgs self sops-nix home-manager networking; };
-        r820 = r820 { inherit nixpkgs self sops-nix home-manager networking; };
+        navi = navi { inherit nixpkgs self sops-nix home-manager; };
+        oryx = oryx { inherit nixpkgs self sops-nix home-manager; };
+        r730 = r730 { inherit nixpkgs self sops-nix home-manager; };
+        r730xd = r730xd { inherit nixpkgs self sops-nix home-manager; };
+        r820 = r820 { inherit nixpkgs self sops-nix home-manager; };
       };
 
+      # User configurations
       homeConfigurations = {
-        "aljam@navi" = aljam { inherit nixpkgs self home-manager networking; };
+        "aljam@navi" = aljam { inherit nixpkgs self home-manager; };
       };
 
+      # Development and utility outputs
       devShells = forAllSystems (system: {
         default = nixpkgs.legacyPackages.${system}.mkShell {
           packages = [
@@ -55,8 +56,10 @@
         };
       });
 
+      # Formatter
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-classic);
 
+      # Pre-commit checks
       pre-commit = {
         check = forAllSystems (system: self.checks.${system}.pre-commit-check);
         config = import ./tests/pre-commit.nix { inherit nixpkgs; };
