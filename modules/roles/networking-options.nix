69dc5226d80fef83f3b5ca5d370c571f0703c631
Aljam <aljam@live.ca>
2026-08-16 02:09:51 -0400

    Update networking options for subnets and domains
    Refactor subnet and domain options for clarity and structure.

--- modules/roles/networking-options.nix
+++ modules/roles/networking-options.nix
@@ -14,23 +14,31 @@
 
     subnets = lib.mkOption {
       type = lib.types.attrs;
-      default = "192.168.1.0/24";
-      lan = "192.168.1";
-      management = "127.0.0.0/8";
+      default = {
+        lan = "192.168.1";
+        management = "127.0.0.0/8";
+      };
       description = "Subnet definitions for the fleet";
     };
 
     domain = lib.mkOption {
       type = lib.types.str;
-      default = options.networking.domain.derezzed;
+      default = "derezzed.info";
       description = "Primary domain for the host";
-      derezzed    = "derezzed.info";
-      fuwa       = "fuwa.space";
-      cybal      = "cybal.org";
-      netrunner  = "netrunner.dev";
-      glow_net   = "glowrunner.network";
-      glow_dev   = "glowrunner.dev";
-      glow_xyz   = "glowrunner.xyz";
+    };
+
+    domains = lib.mkOption {
+      type = lib.types.attrsOf lib.types.str;
+      default = {
+        derezzed = "derezzed.info";
+        fuwa = "fuwa.space";
+        cybal = "cybal.org";
+        netrunner = "netrunner.dev";
+        glow_net = "glowrunner.network";
+        glow_dev = "glowrunner.dev";
+        glow_xyz = "glowrunner.xyz";
+      };
+      description = "All available domains for the fleet";
     };
   };
 }
