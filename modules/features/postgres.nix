a3751f88000dbfbd2bc23667e5c08dde8dbfe366
Aljam <aljam@live.ca>
2026-08-16 07:12:02 -0400

    Fix subnet reference in PostgreSQL configuration

--- modules/features/postgres.nix
+++ modules/features/postgres.nix
@@ -11,7 +11,7 @@
     authentication = lib.mkOverride 10 ''
     local all all peer
     host all all 127.0.0.1/32 scram-sha-256
-    host all all ${subnets.lan}.0/24 scram-sha-256
+    host all all ${options.networking.subnets.lan}.0/24 scram-sha-256
     '';
     ensureDatabases = [ "webscraper" "admin" ];
     ensureUsers = [
