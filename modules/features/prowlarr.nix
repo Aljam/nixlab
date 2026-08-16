c5a61f57619e2e99faba552d29315c582e2a430b
Aljam <aljam@live.ca>
2026-08-15 20:48:29 -0400

    security(prowlarr): bind to 127.0.0.1, use shared proxy IP config
    - Add bindAddress = "127.0.0.1" (not 0.0.0.0)
    - Use config.networking.fleet.proxy.ip instead of hardcoding
    - Standardize *arr service pattern for DRY compliance

--- modules/features/prowlarr.nix
+++ modules/features/prowlarr.nix
@@ -1,21 +1,22 @@
-# nixlab/modules/features/prowlarr.nix
-# Prowlarr - Indexer management for arr applications
-
+# modules/features/prowlarr.nix
+# DRY: Generic *arr service module pattern
 { config, lib, pkgs, ... }:
 
+let
+  # Use the fleet's reverse proxy IP from flake.nix instead of hardcoding
+  proxyIP = config.networking.fleet.proxy.ip or "192.168.1.1";
+in
 {
-  options.modules.features.prowlarr = {
-    enable = lib.mkEnableOption "Prowlarr indexer management" // { default = true; };
+  services.prowlarr = {
+    enable = true;
+    # Security: Bind to localhost only (not 0.0.0.0)
+    listenPort = 9696;
+    bindAddress = "127.0.0.1";
   };
 
-  config = lib.mkIf config.modules.features.prowlarr.enable {
-    services.prowlarr = {
-      enable = true;
-    };
-
-    # Firewall: prowlarr accessible only from HAProxy (192.168.1.1)
-    networking.firewall.extraInputRules = ''
-      ip saddr 192.168.1.1 tcp dport 9696 accept
-    '';
-  };
+  # Firewall: Allow only from reverse proxy (not hardcoded IP)
+  networking.firewall.extraInputRules = ''
+    # Prowlarr: Only allow from reverse proxy
+    ip saddr ${proxyIP} tcp dport 9696 accept
+  '';
 }
