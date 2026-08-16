3594607298272b55ab45816a41c4fa99fe5d7877
Aljam <aljam@live.ca>
2026-08-15 22:23:55 -0400

    Add new cachix cache and trusted public key

--- modules/roles/common.nix
+++ modules/roles/common.nix
@@ -19,12 +19,14 @@
 
   # Set up cachix caches
   nix.settings.substituters = [
+    "https://aljam.cachix.org"
     "https://nix-community.cachix.org"
     "https://nix-gaming.cachix.org"
     "https://hyprland.cachix.org"
     "https://cuda-maintainers.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
+    "aljam.cachix.org-1:E3E80fk8YEaTw0Y9V+0IHmhrvLQ/xACZ1VMXDZZ80oo="
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
