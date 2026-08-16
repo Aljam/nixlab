7cf0c0dadd87b001cea342527e6e5a79dc9f3560
Aljam <aljam@live.ca>
2026-08-16 07:16:21 -0400

    Fix Nix configuration for PostgreSQL and pgAdmin

--- modules/features/postgres.nix
+++ modules/features/postgres.nix
@@ -9,9 +9,9 @@
     enableTCPIP = true;
     settings.password_encryption = "scram-sha-256";
     authentication = lib.mkOverride 10 ''
-    local all all peer
-    host all all 127.0.0.1/32 scram-sha-256
-    host all all ${config.options.networking.subnets.lan}.0/24 scram-sha-256
+      local all all peer
+      host all all 127.0.0.1/32 scram-sha-256
+      host all all ${config.networking.subnets.lan}.0/24 scram-sha-256
     '';
     ensureDatabases = [ "webscraper" "admin" ];
     ensureUsers = [
@@ -28,20 +28,16 @@
 
   services.pgadmin = {
     enable = true;
-    initialEmail = "admin@derezzed.info"; # Using your existing domain
+    initialEmail = "admin@derezzed.info";
     initialPasswordFile = config.sops.secrets.pgadmin_password.path;
-    port = 5050; # Default port for the web interface
-    settings = {
-      DEFAULT_SERVER = "${config.options.servicesHostIP}";
-    };
+    port = 5050;
   };
 
-  systemd.services.pgadmin.environment = {
-    PGADMIN_LISTEN_ADDRESS = "${config.options.servicesHostIP}";
-  };
+  # pgadmin4 doesn't support SERVER_ADDRESS via NixOS module
+  # Just use haproxy to proxy to localhost:5050
 
   networking.firewall.interfaces.eno1.allowedTCPPorts = [ 5432 ];
   networking.firewall.extraInputRules = ''
-    ip saddr ${config.options.networking.fleet.proxy.ip} tcp dport 5050 accept
+    ip saddr ${config.networking.fleet.proxy.ip} tcp dport 5050 accept
   '';
 }
