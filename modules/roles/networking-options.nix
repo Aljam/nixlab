52fb551d5fc0cf20f89c36e012d5ffb2b9dc8940
Aljam <aljam@live.ca>
2026-08-16 02:16:19 -0400

    Refactor networking options and domain definitions

--- modules/roles/networking-options.nix
+++ modules/roles/networking-options.nix
@@ -6,46 +6,54 @@ let
     lan = "192.168.1";
     management = "127.0.0.0/8";
   };
+  
+  domains = {
+    primary = "derezzed.info";
+    derezzed = "derezzed.info";
+    fuwa = "fuwa.space";
+    cybal = "cybal.org";
+    netrunner = "netrunner.dev";
+    glow_net = "glowrunner.network";
+    glow_dev = "glowrunner.dev";
+    glow_xyz = "glowrunner.xyz";
+  };
+  
+  fleet = {
+    navi = { };
+    oryx = { };
+    r820 = { ip = "${subnets.lan}.4"; };
+    r730 = { ip = "${subnets.lan}.3"; zpool = "r730pool"; };
+    r730xd = { ip = "${subnets.lan}.2"; zpool = "mediapool"; };
+    proxy = { ip = "${subnets.lan}.1"; };
+  };
 in
 {
   options.networking = {
     fleet = lib.mkOption {
-      type = lib.types.attrsOf lib.types.attrs;
-      default = {
-        navi = { };
-        oryx = { };
-        r820 = { ip = "${subnets.lan}.4"; };
-        r730 = { ip = "${subnets.lan}.3"; zpool = "r730pool"; };
-        r730xd = { ip = "${subnets.lan}.2"; zpool = "mediapool"; };
-        proxy = { ip = "${subnets.lan}.1"; };
-      };
+      type = lib.types.attrs;
       description = "Fleet host definitions";
     };
 
     subnets = lib.mkOption {
       type = lib.types.attrs;
-      default = subnets;
       description = "Subnet definitions for the fleet";
     };
 
     domain = lib.mkOption {
       type = lib.types.str;
-      default = "derezzed.info";
       description = "Primary domain for the host";
     };
 
     domains = lib.mkOption {
       type = lib.types.attrsOf lib.types.str;
-      default = {
-        derezzed = "derezzed.info";
-        fuwa = "fuwa.space";
-        cybal = "cybal.org";
-        netrunner = "netrunner.dev";
-        glow_net = "glowrunner.network";
-        glow_dev = "glowrunner.dev";
-        glow_xyz = "glowrunner.xyz";
-      };
       description = "All available domains for the fleet";
     };
+  };
+  
+  config.networking = {
+    fleet = fleet;
+    subnets = subnets;
+    domain = domains.primary;
+    domains = domains;
   };
 }
