7f7684c97a9d40f77efdb5d3f099a747758597f2
Aljam <aljam@live.ca>
2026-08-16 05:37:51 -0400

    Add declarative config.xml generation for Readarr

--- modules/features/readarr.nix
+++ modules/features/readarr.nix
@@ -9,5 +9,31 @@ in
     enable = true;
   };
 
-  readarr.environment.READARR__SERVER__BINDADDRESS = "${bindAddr}";
+  # Generate config.xml declaratively with correct port
+  environment.etc."readarr/config.xml".text = ''
+    <Config>
+      <BindAddress>${bindAddr}</BindAddress>
+      <Port>8787</Port>
+      <SslPort>6868</SslPort>
+      <EnableSsl>False</EnableSsl>
+      <LaunchBrowser>True</LaunchBrowser>
+      <ApiKey>71a5fa0bb47d49c88c263cc4954f3b88</ApiKey>
+      <AuthenticationMethod>Forms</AuthenticationMethod>
+      <AuthenticationRequired>Enabled</AuthenticationRequired>
+      <Branch>develop</Branch>
+      <LogLevel>debug</LogLevel>
+      <SslCertPath></SslCertPath>
+      <SslCertPassword></SslCertPassword>
+      <UrlBase></UrlBase>
+      <InstanceName>Readarr</InstanceName>
+    </Config>
+  '';
+
+  systemd.services.readarr = {
+    serviceConfig = {
+      ExecStartPre = [
+        "${pkgs.coreutils}/bin/cp -f /etc/readarr/config.xml /var/lib/readarr/config.xml"
+      ];
+    };
+  };
 }
