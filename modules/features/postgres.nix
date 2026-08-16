ec5153c8b518f5adfaad14f7aa3b42563c52116b
Aljam <aljam@live.ca>
2026-08-16 07:56:47 -0400

    Refactor pgAdmin configuration and service setup
    Updated pgAdmin configuration to disable the NixOS pgAdmin module and create a custom shell wrapper for pgAdmin4. Adjusted ownership of pgAdmin password and added service configuration for pgAdmin4.

--- modules/features/postgres.nix
+++ modules/features/postgres.nix
@@ -1,8 +1,12 @@
 { config, pkgs, lib, ... }:
 
+let
+  hostname = config.networking.hostName;
+  bindAddr = config.networking.fleet.${hostname}.ip;
+in
 {
   sops.secrets.pgadmin_password = {
-    owner = "pgadmin";
+    owner = "postgres";
   };
 
   services.postgresql = {
@@ -27,13 +31,41 @@
     ];
   };
 
-  services.pgadmin = {
-    enable = true;
-    initialEmail = "admin@derezzed.info";
-    initialPasswordFile = config.sops.secrets.pgadmin_password.path;
-    port = 5050;
+  # Disable NixOS pgadmin module
+  services.pgadmin.enable = false;
+  
+  # Create a shell wrapper that sets env and runs pgadmin4
+  environment.etc."pgadmin4-run.sh".text = ''
+    #!/bin/sh
+    export SERVER_ADDRESS=${bindAddr}
+    export PGADMIN_PORT=5050
+    exec ${pkgs.pgadmin4}/bin/pgadmin4
+  '';
+  
+  systemd.services.pgadmin = {
+    description = "pgAdmin4";
+    after = [ "network.target" "postgresql.service" ];
+    wants = [ "network.target" "postgresql.service" ];
+    wantedBy = [ "multi-user.target" ];
+    
+    serviceConfig = {
+      Type = "simple";
+      User = "postgres";
+      Group = "postgres";
+      WorkingDirectory = "/var/lib/pgadmin";
+      ExecStart = "/etc/pgadmin4-run.sh";
+      Restart = "always";
+      Environment = [
+        "SERVER_ADDRESS=${bindAddr}"
+        "PGADMIN_PORT=5050"
+      ];
+    };
+    
+    preStart = ''
+      mkdir -p /var/lib/pgadmin
+      chown postgres:postgres /var/lib/pgadmin
+    '';
   };
 
-  # Allow haproxy (on pfSense) to reach pgadmin
   networking.firewall.interfaces.eno1.allowedTCPPorts = [ 5432 5050 ];
 }
