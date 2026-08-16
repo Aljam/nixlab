323e00946a60511684db071c331833d1fd453bad
Aljam <aljam@live.ca>
2026-08-16 05:01:52 -0400

    Remove BindAddress from readarr configuration
    Removed BindAddress configuration from readarr service.

--- modules/features/readarr.nix
+++ modules/features/readarr.nix
@@ -1,20 +1,8 @@
 # modules/features/readarr.nix
 { config, lib, pkgs, ... }:
 
-let
-  bindAddr = config.servicesHostIP or "127.0.0.1";
-in
 {
   services.readarr = {
     enable = true;
-  };
-
-  # Inject BindAddress into config.xml on start
-  systemd.services.readarr = {
-    serviceConfig = {
-      ExecStartPre = [
-        "${pkgs.gnused}/bin/sed -i 's|</Config>|  <BindAddress>${bindAddr}</BindAddress>\\n</Config>|' /var/lib/readarr/config.xml || true"
-      ];
-    };
   };
 }
