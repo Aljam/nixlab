b15e4b64445aeefc5f0a938f72e9f0cee6839e7d
Aljam <aljam@live.ca>
2026-08-15 21:18:39 -0400

    cleanup(sonarr): remove duplicate firewall rules
    - Remove extraInputRules (handled by reverse-proxy-backends.nix)
    - Keep localhost binding
    - DRY compliance

--- modules/features/sonarr.nix
+++ modules/features/sonarr.nix
@@ -2,10 +2,6 @@
 # DRY: Generic *arr service module pattern
 { config, lib, pkgs, ... }:
 
-let
-  # Use the fleet's reverse proxy IP from flake.nix instead of hardcoding
-  proxyIP = config.networking.fleet.proxy.ip or "192.168.1.1";
-in
 {
   services.sonarr = {
     enable = true;
@@ -14,9 +10,5 @@ in
     bindAddress = "127.0.0.1";
   };
 
-  # Firewall: Allow only from reverse proxy (not hardcoded IP)
-  networking.firewall.extraInputRules = ''
-    # Sonarr: Only allow from reverse proxy
-    ip saddr ${proxyIP} tcp dport 8989 accept
-  '';
+  # Firewall: Handled centrally by reverse-proxy-backends.nix
 }
