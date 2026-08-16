aa7631ff84fc6aa1de4c62f8a84405abdbddbfe0
Aljam <aljam@live.ca>
2026-08-16 02:26:05 -0400

    Update IP address references to use config variable

--- hosts/r820/configuration.nix
+++ hosts/r820/configuration.nix
@@ -11,10 +11,10 @@
   ];
   
   # Set servicesHostIP from fleet for HAProxy backend access
-  servicesHostIP = fleet.r820.ip;
+  servicesHostIP = config.networking.fleet.r820.ip;
   
   networking.interfaces.eno1.ipv4.addresses = [{
-    address = fleet.r820.ip;
+    address = config.networking.fleet.r820.ip;
     prefixLength = 24;
   }];
 
