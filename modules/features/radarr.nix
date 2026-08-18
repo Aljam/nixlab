b367155e4112cf8f8991649955c89bec933ab31f
Aljam <aljam@live.ca>
2026-08-18 15:42:29 -0400

    refactor: declare Radarr proxy port

--- modules/features/radarr.nix
+++ modules/features/radarr.nix
@@ -1,9 +1,10 @@
-# modules/features/radarr.nix
 # Security: Radarr should NOT be exposed to LAN
 # Only accessible via HAProxy gateway (192.168.1.1)
 { config, lib, pkgs, ... }:
 
 {
+  networking.proxyBackendPorts = [ 7878 ];
+
   services.radarr = {
     enable = true;
   };
