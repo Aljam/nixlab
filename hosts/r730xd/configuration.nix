0adce2b9c5a03ede12eb3709f0f930b536cf61a1
Aljam <aljam@live.ca>
2026-08-18 16:19:25 -0400

    Modify configuration for r730xd host
    Updated imports and configuration settings for the r730xd host.

--- hosts/r730xd/configuration.nix
+++ hosts/r730xd/configuration.nix
@@ -1,14 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports = [
-    ../../modules/hardware/dell-r730xd.nix
+    ./hardware-configuration.nix
+    ./disko-config.nix
+    ../../modules/hardware/dell-poweredge.nix
     ../../modules/roles/server-core.nix
-    ../../modules/roles/storage-node.nix
-    ../../modules/roles/media-node.nix
     ../../modules/features/nvidia-headless.nix
+    ../../modules/roles/media-node.nix
+    ../../modules/roles/storage-node.nix
     ../../modules/features/prometheus-server.nix
+    ../../modules/features/grafana.nix 
   ];
 
-  networking.hostId = "<existing-r730xd-host-id>";
+  boot.kernelPackages = pkgs.linuxPackages_6_1;  
+
+  boot.kernelParams = [ "zfs.zfs_arc_max=68719476736" ];
+
+  networking.hostId = "d2083fdc"; # Required for ZFS
 }
