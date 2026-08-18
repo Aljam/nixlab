cf15fae8298f5a1a64293fd238bdc9abe01d3d65
Aljam <aljam@live.ca>
2026-08-18 08:01:01 -0400

    Remove servicesHostIP for HAProxy configuration
    Removed servicesHostIP assignment for HAProxy backend access.

--- hosts/r820/configuration.nix
+++ hosts/r820/configuration.nix
@@ -10,9 +10,6 @@
     #../../modules/roles/mail-node.nix
   ];
   
-  # Set servicesHostIP from fleet for HAProxy backend access
-  servicesHostIP = config.networking.fleet.r820.ip;
-  
   networking.interfaces.eno1.ipv4.addresses = [{
     address = config.networking.fleet.r820.ip;
     prefixLength = 24;
