598409f70d1d0f0875712c9f90c68bdf358c1b7f
Aljam <aljam@live.ca>
2026-08-18 17:25:35 -0400

    Refactor reverse-proxy-backends.nix configuration

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -1,15 +1,7 @@
-{ config, lib, ... }: {
-  options = {
-    networking.proxyBackendPorts = lib.mkOption {
-      type = lib.types.listOf lib.types.port;
-      default = [];
-      description = "Ports that HAProxy/proxy IP is allowed to access on backend services.";
-    };
-  };
+{ config, lib, ... }:
 
-  config = {
-    networking.firewall.extraInputRules = lib.mkIf (config.networking.proxyBackendPorts != []) ''
-      ip saddr ${config.fleet.proxy.ip} tcp dport { ${lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts)} } accept comment "HAProxy backend access"
-    '';
-  };
+{
+  networking.firewall.extraInputRules = lib.mkIf (config.networking.proxyBackendPorts != []) ''
+    ip saddr ${config.fleet.proxy.ip} tcp dport { ${lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts)} } accept comment "HAProxy backend access"
+  '';
 }
