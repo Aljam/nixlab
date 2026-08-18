dab3e28324acf4b189190365a4c66ce98dfcb291
Aljam <aljam@live.ca>
2026-08-18 15:40:51 -0400

    refactor: declare Prowlarr proxy port

--- modules/features/prowlarr.nix
+++ modules/features/prowlarr.nix
@@ -1,9 +1,10 @@
-# modules/features/prowlarr.nix
 # Security: Prowlarr should NOT be exposed to LAN
 # Only accessible via HAProxy gateway (192.168.1.1)
 { config, lib, pkgs, ... }:
 
 {
+  networking.proxyBackendPorts = [ 9696 ];
+
   services.prowlarr = {
     enable = true;
   };
