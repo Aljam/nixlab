c522d3cb28a36a009d8d3fb081b1dd51f3b8b046
Aljam <aljam@live.ca>
2026-08-18 17:28:16 -0400

    Fix typo in proxy backend IP reference

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -2,6 +2,6 @@
 
 {
   networking.firewall.extraInputRules = lib.mkIf (config.networking.proxyBackendPorts != []) ''
-    ip saddr ${config.fleet.proxy.ip} tcp dport { ${lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts)} } accept comment "HAProxy backend access"
+    ip saddr ${config.networking.fleet.proxy.ip} tcp dport { ${lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts)} } accept comment "HAProxy backend access"
   '';
 }
