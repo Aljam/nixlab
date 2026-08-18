82f66f903d64facfdc71542727a2a065afd723ef
Aljam <aljam@live.ca>
2026-08-18 16:21:22 -0400

    Remove grafana module from configuration
    Removed grafana module from the configuration.

--- hosts/r730xd/configuration.nix
+++ hosts/r730xd/configuration.nix
@@ -10,7 +10,6 @@
     ../../modules/roles/media-node.nix
     ../../modules/roles/storage-node.nix
     ../../modules/features/prometheus-server.nix
-    ../../modules/features/grafana.nix 
   ];
 
   boot.kernelPackages = pkgs.linuxPackages_6_1;  
