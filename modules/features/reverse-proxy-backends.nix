5d2fcb2594562a348c92faa5dfafcfe383519b32
Aljam <aljam@live.ca>
2026-08-18 15:03:45 -0400

    refactor: consume HAProxy backend port registry

--- modules/features/reverse-proxy-backends.nix
+++ modules/features/reverse-proxy-backends.nix
@@ -1,16 +1,14 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 let
   proxyIP = config.networking.fleet.proxy.ip or "192.168.1.1";
+  backendPorts = lib.concatStringsSep ", " (map toString config.networking.proxyBackendPorts);
 in
 {
   networking.firewall = {
     extraInputRules = ''
-      ip saddr ${proxyIP} tcp dport {
-        3000, 5050, 5055, 6767, 7474, 8082, 8111, 9090, 9093,
-        9100, 13378, 8222, 8989, 8686, 8787, 7878, 9696, 8080,
-        8081, 8096
-      } accept comment "HAProxy backend access"
+      ip saddr ${proxyIP} tcp dport { ${backendPorts} }
+        accept comment "HAProxy backend access"
     '';
   };
 }
