63477d5a6c249ab2f7f1edf5563e071fcfce6310
Aljam <aljam@live.ca>
2026-08-20 15:22:36 -0400

    Add extractor_args for YouTube player client

--- modules/features/youtube.nix
+++ modules/features/youtube.nix
@@ -29,13 +29,18 @@ in
             deno: {}
           remote_components:
             - "ejs:github"
+          extractor_args:
+            youtube:
+              player_client:
+                - "default"
+                - "-android_sdkless"
           sleep_requests: 5
           sleep_interval: 60
           max_sleep_interval: 180
           retries: 3
           fragment_retries: 3
           ignoreerrors: true
-
+        
         output_options:
           output_directory: "/mnt/media/youtube"
           file_name: "{channel}/{upload_date}_{title}.{ext}"
